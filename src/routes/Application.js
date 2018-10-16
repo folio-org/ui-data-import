336afb21cd87f751448475fb3eece269b0c71e91
oleksii-popov <oleksii_popov@epam.com>
2018-10-16 15:12:53 +0300

    UIDATIMP-20 Eslint fix

--- src/routes/Application.js
+++ src/routes/Application.js
@@ -1,62 +1,62 @@
 import React from 'react';
-import {Button, Pane, Paneset, PaneMenu } from '@folio/stripes/components';
+import { Button, Pane, Paneset, PaneMenu } from '@folio/stripes/components';
 
 export default class Application extends React.Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            filterPaneIsVisible: true,
-            recordDetailsPaneIsVisible: true,
-        };
-    }
+    this.state = {
+      filterPaneIsVisible: true,
+      recordDetailsPaneIsVisible: true,
+    };
+  }
 
-    addResultsFirstMenu() {
-        const buttonRightStyle = {
-            position: 'absolute',
-            right: 0
-        };
-        return (
-            <PaneMenu>
-                <Button
-                    id="clickable-new-12"
-                    href="#"
-                    buttonStyle="primary paneHeaderNewButton"
-                    marginBottom0
-                    style={buttonRightStyle}
-                >
+  addResultsFirstMenu() {
+    const buttonRightStyle = {
+      position: 'absolute',
+      right: 0
+    };
+
+
+    return (
+      <PaneMenu>
+        <Button
+          id="clickable-new-12"
+          href="#"
+          buttonStyle="primary paneHeaderNewButton"
+          marginBottom0
+          style={buttonRightStyle}
+        >
                     Manage
-                </Button>
-            </PaneMenu>
-        );
-    }
+        </Button>
+      </PaneMenu>
+    );
+  }
 
-    render() {
-        const buttonRightStyle = {
-            position: 'absolute',
-            right: 0
-        };
+  render() {
+    const buttonRightStyle = {
+      position: 'absolute',
+      right: 0
+    };
 
-        return (
-            <Paneset>
-                <Pane defaultWidth="20" paneTitle="Jobs" firstMenu={this.addResultsFirstMenu()}>
-                </Pane>
-                <Pane
-                    defaultWidth="fill"
-                    paneTitle={<div>Logs
-                        <Button
-                            style={buttonRightStyle}
-                            id="clickable-new-12"
-                            href="#"
-                            buttonStyle="primary paneHeaderNewButton"
-                            marginBottom0
-                        >View all</Button>
-                    </div>}
-                >
-                </Pane>
-                <Pane defaultWidth="fill" paneTitle="Import">
-                </Pane>
-            </Paneset>
-        );
-    }
-};
+    return (
+      <Paneset>
+        <Pane defaultWidth="20" paneTitle="Jobs" firstMenu={this.addResultsFirstMenu()} />
+        <Pane
+          defaultWidth="fill"
+          paneTitle={<div>Logs
+            <Button
+              style={buttonRightStyle}
+              id="clickable-new-12"
+              href="#"
+              buttonStyle="primary paneHeaderNewButton"
+              marginBottom0
+            >View all
+            </Button>
+          </div>}
+        />
+        <Pane defaultWidth="fill" paneTitle="Import" />
+      </Paneset>
+    );
+  }
+}
