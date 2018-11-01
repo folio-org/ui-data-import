52379d525b617e59ce7114968b3ff3af9ce596e6
mykola_yenin <mykola_yenin@epam.com>
2018-11-01 18:02:33 +0200

    Implemented styling of drag-n-drop in process.

--- src/components/ImportJobs/ImportJobs.js
+++ src/components/ImportJobs/ImportJobs.js
@@ -2,13 +2,53 @@ import React from 'react';
 import { FormattedMessage } from 'react-intl';
 
 import FileUpload from './components/FileUpload';
+import css from './components/FileUpload/FileUpload.css';
+
 
 class ImportJobs extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      isDropZoneActive: false
+    };
+  }
+
+  onDragEnter = () => {
+    this.setState({ isDropZoneActive: true });
+  };
+
+  onDragLeave = () => {
+    this.setState({ isDropZoneActive: false });
+  };
+
+  onDrop = (acceptedFiles, rejectedFiles) => {
+    console.log(acceptedFiles);
+    this.setState({
+      isDropZoneActive: false
+    });
+  };
+
+
+  getTitleMessageId = () => {
+    if (this.state.isDropZoneActive) {
+      return 'ui-data-import.activeUploadTitle';
+    }
+
+    return 'ui-data-import.uploadTitle';
+  };
+
+
   render() {
     return (
       <FileUpload
-        title={<FormattedMessage id="ui-data-import.uploadTitle" />}
+        title={<FormattedMessage id={this.getTitleMessageId()} />}
         uploadBtnText={<FormattedMessage id="ui-data-import.uploadBtnText" />}
+        isDropZoneActive={this.state.isDropZoneActive}
+        onDragEnter={this.onDragEnter}
+        onDragLeave={this.onDragLeave}
+        onDrop={this.onDrop}
+        className={css.upload}
+        activeClassName={css.activeUpload}
       />
     );
   }
--- src/components/ImportJobs/components/FileUpload/FileUpload.js
+++ src/components/ImportJobs/components/FileUpload/FileUpload.js
@@ -1,35 +1,121 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import noop from 'lodash/noop';
-
+import ReactDropzone from 'react-dropzone';
 import { Button } from '@folio/stripes/components';
 
+import classNames from 'classnames/bind';
 import css from './FileUpload.css';
 
-class FileUpload extends React.Component {
-  static propTypes = {
-    title: PropTypes.string.isRequired,
-    uploadBtnText: PropTypes.string.isRequired,
+
+const cx = classNames.bind(css);
+
+const FileUpload = (props) => {
+  const {
+    title,
+    uploadBtnText,
+    accept,
+    onDrop,
+    onDragEnter,
+    onDragLeave,
+    isDropZoneActive,
+    className,
+    acceptClassName,
+    activeClassName,
+    rejectClassName,
+    disabledClassName,
+    maxSize,
+    getDataTransferItems,
+    children,
+    style
+  } = props;
+
+  const initStyle = {
+    width: '300px',
+    height: '300px',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'column',
+    textAlign: 'center',
+    border: '1px solid black'
   };
 
-  render() {
-    const {
-      title,
-      uploadBtnText,
-    } = this.props;
+  const getTitleClassName = dropZoneState => {
+    return cx({
+      uploadTitle: true,
+      activeUploadTitle: dropZoneState
+    });
+  };
 
-    return (
-      <div className={css.upload}>
-        <span className={css.uploadTitle}>{title}</span>
-        <Button
-          buttonStyle="primary"
-          onClick={noop}
-        >
-          {uploadBtnText}
-        </Button>
-      </div>
-    );
-  }
-}
+  const getUsedStyle = (styleFromProps, classNameFromProps, initialStyle) => {
+    if (styleFromProps) return styleFromProps;
+    if (!styleFromProps && classNameFromProps) return null;
+
+    return initialStyle;
+  };
+
+  const titleClassName = getTitleClassName(isDropZoneActive);
+
+
+  const usedStyle = getUsedStyle(style, className, initStyle);
+
+
+  return (
+    <ReactDropzone
+      className={className || null}
+      style={usedStyle}
+      activeClassName={activeClassName}
+      onDrop={onDrop}
+      onDragEnter={onDragEnter}
+      onDragLeave={onDragLeave}
+      disableClick
+      accept={accept}
+      acceptClassName={acceptClassName}
+      rejectClassName={rejectClassName}
+      disabledClassName={disabledClassName}
+      maxSize={maxSize}
+      getDataTransferItems={getDataTransferItems}
+    >
+      {({ open }) => (
+        <React.Fragment>
+          {children}
+          <span className={titleClassName}>{title}</span>
+          {!isDropZoneActive &&
+          <Button
+            buttonStyle="primary"
+            onClick={() => open()}
+          >
+            {uploadBtnText}
+          </Button>}
+        </React.Fragment>
+      )}
+    </ReactDropzone>
+  );
+};
+
+FileUpload.propTypes = {
+  title: PropTypes.string.isRequired,
+  uploadBtnText: PropTypes.string.isRequired,
+  isDropZoneActive: PropTypes.bool.isRequired,
+  onDrop: PropTypes.func.isRequired,
+  onDragEnter: PropTypes.func,
+  onDragLeave: PropTypes.func,
+  className: PropTypes.string,
+  acceptClassName: PropTypes.string,
+  activeClassName: PropTypes.string,
+  rejectClassName: PropTypes.string,
+  disabledClassName: PropTypes.string,
+  maxSize: PropTypes.number,
+  style: PropTypes.object,
+  getDataTransferItems: PropTypes.func,
+  accept: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.arrayOf(PropTypes.string)
+  ]),
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node
+  ])
+};
 
 export default FileUpload;
