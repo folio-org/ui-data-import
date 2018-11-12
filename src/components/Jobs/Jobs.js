4672224883ebc24cd1a341cb5888020d44c4cd72
OleksandrAntonenko1 <42577309+OleksandrAntonenko1@users.noreply.github.com>
2018-11-12 11:44:15 -0600

    Use multi-line import for react-intl
    Co-Authored-By: cherewaty <jeffrey@cherewaty.com>

--- src/components/Jobs/Jobs.js
+++ src/components/Jobs/Jobs.js
@@ -1,5 +1,9 @@
 import React from 'react';
-import { FormattedMessage, injectIntl, intlShape } from 'react-intl';
+import { 
+  FormattedMessage, 
+  injectIntl, 
+  intlShape,
+} from 'react-intl';
 
 import {
   AccordionSet,
