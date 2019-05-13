168b698fae89b67a7d9cc42a31a2ba4a2a1a2a0e
Taras Tkachenko <taras_tkachenko@epam.com>
2019-05-13 18:16:23 +0300

    UIDATIMP-7: Resolve PR comments :: DONE

--- test/bigtest/tests/html-decode-test.js
+++ test/bigtest/tests/html-decode-test.js
@@ -8,6 +8,6 @@ import { htmlDecode } from '../../../src/utils';
 
 describe('htmlDecode function', () => {
   it('decodes HTML special chars in the given string', () => {
-    expect(htmlDecode('Order&nbsp;&middot;&nbsp;990&nbsp;&rarr;&nbsp;PO Line Number')).to.equal('Order · 990 → PO Line Number');
+    expect(htmlDecode('Order&nbsp;&middot;&nbsp;990&nbsp;&rarr;&nbsp;PO Line Number')).to.equal('Order · 990 → PO Line Number');
   });
 });
