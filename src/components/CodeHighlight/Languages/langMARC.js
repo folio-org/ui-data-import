d86a03d81030da1571f39f054e682e375ca6a100
Taras Tkachenko <taras_tkachenko@epam.com>
2019-06-11 23:40:12 +0300

    UIDATIMP-209: Fix test mockups :: DONE

--- src/components/CodeHighlight/Languages/langMARC.js
+++ src/components/CodeHighlight/Languages/langMARC.js
@@ -1,19 +1,3 @@
 export const langMARC = code => {
-  const codeString = typeof code === 'string' ? code : JSON.stringify(code, null, 2);
-  const json = codeString.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
-  const re = /("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+-]?\d+)?)/g;
-
-  return json.replace(re, match => {
-    let cls = 'number';
-
-    if (/^"/.test(match)) {
-      cls = /:$/.test(match) ? 'key' : 'string';
-    } else if (/true|false/.test(match)) {
-      cls = 'boolean';
-    } else if (/null/.test(match)) {
-      cls = 'null';
-    }
-
-    return `<span class="${cls}">${match}</span>`;
-  });
+  return code;
 };
--- test/bigtest/mocks/job-log.js
+++ test/bigtest/mocks/job-log.js
@@ -1,220 +1,218 @@
 const jobLogData = [
   {
-    "id": "6a5fa452-b604-4609-b8e2-68b549039d72",
-    "snapshotId": "09dd9aff-1f7f-4bd5-9b30-bd012545ecbb",
-    "matchedId": "e40e7df0-9740-4d95-b9ca-c60b70f8a750",
-    "generation": 0,
-    "recordType": "MARC",
-    "rawRecord": {
-        "id": "19c2272a-e284-4871-8003-4c86b1084b7a",
-        "content": "00423nam  22001335  4500003000800000008004100008020001800049040002100067245007900088"
+    id: '6a5fa452-b604-4609-b8e2-68b549039d72',
+    snapshotId: '09dd9aff-1f7f-4bd5-9b30-bd012545ecbb',
+    matchedId: 'e40e7df0-9740-4d95-b9ca-c60b70f8a750',
+    generation: 0,
+    recordType: 'MARC',
+    rawRecord: {
+      id: '19c2272a-e284-4871-8003-4c86b1084b7a',
+      content: '00423nam  22001335  4500003000800000008004100008020001800049040002100067245007900088',
     },
-    "parsedRecord": {
-      "id": "3a96ab3e-fba3-4a98-a261-9c922ce8e084",
-      "content": {
-        "fields": [
-          {"003": "NhCcYBP"},
-          {"008": "070815s2007    nyua     b    001 0 eng d"},
+    parsedRecord: {
+      id: '3a96ab3e-fba3-4a98-a261-9c922ce8e084',
+      content: {
+        fields: [
+          { '003': 'NhCcYBP' },
+          { '008': '070815s2007    nyua     b    001 0 eng d' },
           {
-            "020": {
-              "ind1": " ",
-              "ind2": " ",
-              "subfields": [
-                {"a": "9780387360331"}
-              ]
-            }
+            '020': {
+              ind1: ' ',
+              ind2: ' ',
+              subfields: [
+                { a: '9780387360331' },
+              ],
+            },
           }, {
-            "040": {
-              "ind1": " ",
-              "ind2": " ",
-              "subfields": [
-                {"a": "NhCcYBP"},
-                {"c": "NhCcYBP"}
-              ]
-            }
+            '040': {
+              ind1: ' ',
+              ind2: ' ',
+              subfields: [
+                { a: 'NhCcYBP' },
+                { c: 'NhCcYBP' },
+              ],
+            },
           }, {
-            "245": {
-              "ind1": "0",
-              "ind2": "0",
-              "subfields": [
-                {"a": "New computational paradigms :"},
-                {"b": "changing conceptions of what is computable."}
-              ]
-            }
+            245: {
+              ind1: '0',
+              ind2: '0',
+              subfields: [
+                { a: 'New computational paradigms :' },
+                { b: 'changing conceptions of what is computable.' },
+              ],
+            },
           }, {
-            "260": {
-              "ind1": " ",
-              "ind2": " ",
-              "subfields": [
-                {"a": "New York :"},
-                {"b": "Springer,"},
-                {"c": "2008."}
-              ]
-            }
+            260: {
+              ind1: ' ',
+              ind2: ' ',
+              subfields: [
+                { a: 'New York :' },
+                { b: 'Springer,' },
+                { c: '2008.' },
+              ],
+            },
           }, {
-            "700": {
-              "ind1": "1",
-              "ind2": " ",
-              "subfields": [
-                {"a": "Cooper, S. B."},
-                {"q": "(S. Barry)"}
-              ]
-            }
+            700: {
+              ind1: '1',
+              ind2: ' ',
+              subfields: [
+                { a: 'Cooper, S. B.' },
+                { q: '(S. Barry)' },
+              ],
+            },
           }, {
-            "980": {
-              "ind1": " ",
-              "ind2": " ",
-              "subfields": [
-                {"a": "080313"},
-                {"b": "13900"},
-                {"d": "000"},
-                {"e": "11329"},
-                {"f": "362718"}
-              ]
-            }
+            980: {
+              ind1: ' ',
+              ind2: ' ',
+              subfields: [
+                { a: '080313' },
+                { b: '13900' },
+                { d: '000' },
+                { e: '11329' },
+                { f: '362718' },
+              ],
+            },
           }, {
-            "981": {
-              "ind1": " ",
-              "ind2": " ",
-              "subfields": [
-                {"a": "ya26M"},
-                {"b": "zybpw"},
-                {"c": "dd"}
-              ]
-            }
+            981: {
+              ind1: ' ',
+              ind2: ' ',
+              subfields: [
+                { a: 'ya26M' },
+                { b: 'zybpw' },
+                { c: 'dd' },
+              ],
+            },
           }, {
-            "999": {
-              "ind1": "f",
-              "ind2": "f",
-              "subfields": [
-                {"s": "6a5fa452-b604-4609-b8e2-68b549039d72"},
-                {"i": "8e098dd4-a642-4590-8301-dff3380b4305"}
-              ]
-            }
-          }
+            999: {
+              ind1: 'f',
+              ind2: 'f',
+              subfields: [
+                { s: '6a5fa452-b604-4609-b8e2-68b549039d72' },
+                { i: '8e098dd4-a642-4590-8301-dff3380b4305' },
+              ],
+            },
+          },
         ],
-        "leader": "00423nam  22001335  4500"
-      }
+        leader: '00423nam  22001335  4500',
+      },
     },
-    "deleted": false,
-    "additionalInfo": {"suppressDiscovery": false}
+    deleted: false,
+    additionalInfo: { suppressDiscovery: false },
   }, {
-    "id": "c3b9fc48-b752-4616-9c82-02816dc9f38c",
-    "snapshotId": "09dd9aff-1f7f-4bd5-9b30-bd012545ecbb",
-    "matchedId": "e0f01121-03b5-4a4d-94a3-24653de7268a",
-    "generation": 0,
-    "recordType": "MARC",
-    "rawRecord": {
-      "id": "8a965d78-e0e5-4672-9813-8d6fea54cd30",
-      "content": "00499nam  22001455  45000030008000000080041000080200018000490400021000672450039000882600043"
+    id: 'c3b9fc48-b752-4616-9c82-02816dc9f38c',
+    snapshotId: '09dd9aff-1f7f-4bd5-9b30-bd012545ecbb',
+    matchedId: 'e0f01121-03b5-4a4d-94a3-24653de7268a',
+    generation: 0,
+    recordType: 'MARC',
+    rawRecord: {
+      id: '8a965d78-e0e5-4672-9813-8d6fea54cd30',
+      content: '00499nam  22001455  45000030008000000080041000080200018000490400021000672450039000882600043',
     },
-    "errorRecord": {
-      "id": "86e0e28f-4ab9-4393-8bae-2a3f67537dce",
-      "description": "{\"errors\":[{\"name\":\"org.marc4j.MarcException\",\"message\":\"error parsing data field for tag: 700 with data: 1 \\u001FaBuescu, Helena Carvalh�ao\"}]}",
-      "content": "00499nam  22001455  45000030008000000080041000080200018000490400021000672450039000882600043"
+    errorRecord: {
+      id: '86e0e28f-4ab9-4393-8bae-2a3f67537dce',
+      description: '{\'errors\':[{\'name\':\'org.marc4j.MarcException\',\'message\':\'error parsing data field for tag: 700 with data: 1 \\u001FaBuescu, Helena Carvalh�ao\'}]}',
+      content: '00499nam  22001455  45000030008000000080041000080200018000490400021000672450039000882600043',
     },
-    "deleted": false,
-    "additionalInfo": {
-      "suppressDiscovery": false
-    }
+    deleted: false,
+    additionalInfo: { suppressDiscovery: false },
   }, {
-    "id": "5dcfd746-d821-4f6f-b610-241b3e9f3179",
-    "snapshotId": "09dd9aff-1f7f-4bd5-9b30-bd012545ecbb",
-    "matchedId": "247172e5-c296-43a7-9190-a46d42996bda",
-    "generation": 0,
-    "recordType": "MARC",
-    "rawRecord": {
-      "id": "282eeeb7-4ae7-4b1f-b242-e5f5c433eb78",
-      "content": "00409nam  22001335  45000030008000000080041000080200018000490400021000672450068000882600034"
+    id: '5dcfd746-d821-4f6f-b610-241b3e9f3179',
+    snapshotId: '09dd9aff-1f7f-4bd5-9b30-bd012545ecbb',
+    matchedId: '247172e5-c296-43a7-9190-a46d42996bda',
+    generation: 0,
+    recordType: 'MARC',
+    rawRecord: {
+      id: '282eeeb7-4ae7-4b1f-b242-e5f5c433eb78',
+      content: '00409nam  22001335  45000030008000000080041000080200018000490400021000672450068000882600034',
     },
-    "parsedRecord": {
-      "id": "473947a8-a4bf-4d30-8a1c-8533450abcdd",
-      "content": {
-        "fields": [
-          {"003": "NhCcYBP"},
-          {"008": "080310s2008    nyu                 eng d"},
+    parsedRecord: {
+      id: '473947a8-a4bf-4d30-8a1c-8533450abcdd',
+      content: {
+        fields: [
+          { '003': 'NhCcYBP' },
+          { '008': '080310s2008    nyu                 eng d' },
           {
-            "020": {
-              "ind1": " ",
-              "ind2": " ",
-              "subfields": [
-                {"a": "9780387745077"}
-              ]
-            }
+            '020': {
+              ind1: ' ',
+              ind2: ' ',
+              subfields: [
+                { a: '9780387745077' },
+              ],
+            },
           }, {
-            "040": {
-              "ind1": " ",
-              "ind2": " ",
-              "subfields": [
-                {"a": "NhCcYBP"},
-                {"c": "NhCcYBP"}
-              ]
-            }
+            '040': {
+              ind1: ' ',
+              ind2: ' ',
+              subfields: [
+                { a: 'NhCcYBP' },
+                { c: 'NhCcYBP' },
+              ],
+            },
           }, {
-            "245": {
-              "ind1": "0",
-              "ind2": "0",
-              "subfields": [
-                {"a": "Violence in Europe :"},
-                {"b": "historical and contemporary perspectives."}
-              ]
-            }
+            245: {
+              ind1: '0',
+              ind2: '0',
+              subfields: [
+                { a: 'Violence in Europe :' },
+                { b: 'historical and contemporary perspectives.' },
+              ],
+            },
           }, {
-            "260": {
-              "ind1": " ",
-              "ind2": " ",
-              "subfields": [
-                {"a": "New York :"},
-                {"b": "Springer,"},
-                {"c": "c2008."}
-              ]
-            }
+            260: {
+              ind1: ' ',
+              ind2: ' ',
+              subfields: [
+                { a: 'New York :' },
+                { b: 'Springer,' },
+                { c: 'c2008.' },
+              ],
+            },
           }, {
-            "700": {
-              "ind1": "1",
-              "ind2": " ",
-              "subfields": [
-                {"a": "Body-Gendrot, Sophie."}
-              ]
-            }
+            700: {
+              ind1: '1',
+              ind2: ' ',
+              subfields: [
+                { a: 'Body-Gendrot, Sophie.' },
+              ],
+            },
           }, {
-            "980": {
-              "ind1": " ",
-              "ind2": " ",
-              "subfields": [
-                {"a": "080313"},
-                {"b": "15900"},
-                {"d": "000"},
-                {"e": "12959"},
-                {"f": "362718"}
-              ]
-            }
+            980: {
+              ind1: ' ',
+              ind2: ' ',
+              subfields: [
+                { a: '080313' },
+                { b: '15900' },
+                { d: '000' },
+                { e: '12959' },
+                { f: '362718' },
+              ],
+            },
           }, {
-            "981": {
-              "ind1": " ",
-              "ind2": " ",
-              "subfields": [
-                {"a": "ya26M"},
-                {"b": "zybpw"},
-                {"c": "dd"}
-              ]
-            }
+            981: {
+              ind1: ' ',
+              ind2: ' ',
+              subfields: [
+                { a: 'ya26M' },
+                { b: 'zybpw' },
+                { c: 'dd' },
+              ],
+            },
           }, {
-            "999": {
-              "ind1": "f",
-              "ind2": "f",
-              "subfields": [
-                {"s": "5dcfd746-d821-4f6f-b610-241b3e9f3179"},
-                {"i": "492cde62-a1cf-40b2-84d6-a9ba3314e5ef"}
-              ]
-            }
-          }
+            999: {
+              ind1: 'f',
+              ind2: 'f',
+              subfields: [
+                { s: '5dcfd746-d821-4f6f-b610-241b3e9f3179' },
+                { i: '492cde62-a1cf-40b2-84d6-a9ba3314e5ef' },
+              ],
+            },
+          },
         ],
-        "leader": "00409nam  22001335  4500"
-      }
+        leader: '00409nam  22001335  4500',
+      },
     },
-    "deleted": false,
-    "additionalInfo": {"suppressDiscovery": false}
+    deleted: false,
+    additionalInfo: { suppressDiscovery: false },
   },
 ];
 
