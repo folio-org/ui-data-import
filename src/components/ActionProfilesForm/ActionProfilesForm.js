a7be69c734fa121cb7a088924957a53e780b6d73
OleksandrYehorovEPAM <oleksandr_yehorov@epam.com>
2019-07-02 17:28:56 +0300

    UIDATIMP-149: create edit action profile form

--- src/components/ActionProfilesForm/ActionProfilesForm.js
+++ src/components/ActionProfilesForm/ActionProfilesForm.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import queryString from 'query-string';
 import { FormattedMessage } from 'react-intl';
 import { Field } from 'redux-form';
 
@@ -14,6 +15,7 @@ import stripesForm from '@folio/stripes/form';
 
 import { FullScreenForm } from '../FullScreenForm';
 import { validateRequiredField } from '../../utils';
+import { LAYER_TYPES } from '../../utils/constants';
 
 const formName = 'actionProfilesForm';
 
@@ -21,21 +23,42 @@ export const ActionProfilesFormComponent = props => {
   const {
     pristine,
     submitting,
+    initialValues,
     handleSubmit,
+    location: { search },
     onCancel,
   } = props;
 
+  const { layer } = queryString.parse(search);
+  const isEditMode = layer === LAYER_TYPES.EDIT;
   const isSubmitDisabled = pristine || submitting;
 
-  const title = <FormattedMessage id="ui-data-import.settings.actionProfiles.new" />;
+  const paneTitle = isEditMode
+    ? (
+      <FormattedMessage id="ui-data-import.edit">
+        {txt => `${txt} ${initialValues.name}`}
+      </FormattedMessage>
+    )
+    : <FormattedMessage id="ui-data-import.settings.actionProfiles.new" />;
+  const headLine = isEditMode
+    ? initialValues.name
+    : <FormattedMessage id="ui-data-import.settings.actionProfiles.new" />;
+
+  const onSubmit = e => {
+    if (isEditMode) {
+      e.preventDefault();
+    } else {
+      handleSubmit(e);
+    }
+  };
 
   return (
     <FullScreenForm
       id="action-profiles-form"
-      paneTitle={title}
+      paneTitle={paneTitle}
       submitMessage={<FormattedMessage id="ui-data-import.save" />}
       isSubmitDisabled={isSubmitDisabled}
-      onSubmit={handleSubmit}
+      onSubmit={onSubmit}
       onCancel={onCancel}
     >
       <Headline
@@ -43,7 +66,7 @@ export const ActionProfilesFormComponent = props => {
         tag="h2"
         data-test-header-title
       >
-        {title}
+        {headLine}
       </Headline>
       <AccordionSet>
         <Accordion
@@ -75,21 +98,23 @@ export const ActionProfilesFormComponent = props => {
             {/* will be implemented in https://issues.folio.org/browse/UIDATIMP-207 */}
           </div>
         </Accordion>
-        <Accordion
-          label={<FormattedMessage id="ui-data-import.settings.actionProfiles.associatedMappingProfile" />}
-          separator={false}
-        >
-          <div>
-            {/* will be implemented in https://issues.folio.org/browse/UIDATIMP-208 */}
-          </div>
-        </Accordion>
+        {isEditMode || (
+          <Accordion
+            id="actionProfileFormAssociatedMappingProfileAccordion"
+            label={<FormattedMessage id="ui-data-import.settings.actionProfiles.associatedMappingProfile" />}
+            separator={false}
+          >
+            <div>
+              {/* will be implemented in https://issues.folio.org/browse/UIDATIMP-208 */}
+            </div>
+          </Accordion>
+        )}
       </AccordionSet>
     </FullScreenForm>
   );
 };
 
 ActionProfilesFormComponent.propTypes = {
-  // eslint-disable-next-line react/no-unused-prop-types
   initialValues: PropTypes.object.isRequired,
   pristine: PropTypes.bool.isRequired,
   submitting: PropTypes.bool.isRequired,
--- test/bigtest/interactors/action-profiles/action-profiles-form.js
+++ test/bigtest/interactors/action-profiles/action-profiles-form.js
@@ -1,11 +1,13 @@
 import TextFieldInteractor from '@folio/stripes-components/lib/TextField/tests/interactor';
 import TextAreaInteractor from '@folio/stripes-components/lib/TextArea/tests/interactor';
+import { AccordionInteractor } from '@folio/stripes-components/lib/Accordion/tests/interactor';
 
 import { FullScreenFormInteractor } from '../full-screen-form';
 
 class ActionProfileFormInteractor extends FullScreenFormInteractor {
   nameField = new TextFieldInteractor('[data-test-name-field]');
   descriptionField = new TextAreaInteractor('[data-test-description-field]');
+  associatedMappingProfileAccordion = new AccordionInteractor('#actionProfileFormAssociatedMappingProfileAccordion');
 }
 
 export const actionProfileForm = new ActionProfileFormInteractor('[data-test-full-screen-form]');
