29650f02dde71919ac85b8cccf36968722e41046
Vladyslav-Velytskyi <55701515+Vladyslav-Velytskyi@users.noreply.github.com>
2019-12-04 21:30:35 +0200

    UIDATIMP-327: Add tests for section component (#366)

--- test/bigtest/interactors/match-criteria-interactor.js
+++ test/bigtest/interactors/match-criteria-interactor.js
@@ -1,11 +1,16 @@
 import {
   interactor,
   is,
+  collection,
 } from '@bigtest/interactor';
+
 import { AccordionInteractor } from '@folio/stripes-components/lib/Accordion/tests/interactor';
 import KeyValueInteractor from '@folio/stripes-components/lib/KeyValue/tests/interactor';
 import SelectInteractor from '@folio/stripes-components/lib/Select/tests/interactor';
 import { InputInteractor } from './input-interactor';
+
+import { SectionInteractor } from './section-interactor';
+import { CheckboxInteractor } from './checkbox-interactor';
 
 @interactor
 class MatchCriterion {
@@ -13,7 +18,14 @@ class MatchCriterion {
   dropdown = new SelectInteractor('#criterion1-criterion-type');
 }
 
+@interactor
+class RecordSections {
+  children = collection('section', SectionInteractor);
+}
 export class MatchCriteriaInteractor extends AccordionInteractor {
+  matchCriterionSection = new MatchCriterion('[class*="criterion--"] div');
+
+  incomingMarcRecord = new SectionInteractor('section[class*=incoming]');
   fieldMain = new KeyValueInteractor('[data-test-field-main]');
   fieldIn1 = new KeyValueInteractor('[data-test-field-in1]');
   fieldIn2 = new KeyValueInteractor('[data-test-field-in2]');
@@ -22,6 +34,15 @@ export class MatchCriteriaInteractor extends AccordionInteractor {
   inputIn1 = new InputInteractor('[data-test-field-in1]');
   inputIn2 = new InputInteractor('[data-test-field-in2]');
   inputSubfield = new InputInteractor('[data-test-field-subfield');
-  matchCriterionSection = new MatchCriterion('[class*="criterion--"] div');
+  incomingMarcRecordSections = new RecordSections('section[class*=incoming]');
+  incomingMarcRecordQualifierCheckbox = new CheckboxInteractor('section[class*=incoming] [class*="qualifier---"]');
+  incomingMarcRecordPartCheckbox = new CheckboxInteractor('section[class*=incoming] [class*="part---"]');
+
+  matchCriterion = new SectionInteractor('section[class*=" criterion"]');
   matchCriterionField = new KeyValueInteractor('[data-test-match-criterion]');
+
+  existingRecord = new SectionInteractor('section[class*=existing]');
+  existingRecordSections = new RecordSections('section[class*=existing]');
+  existingRecordQualifierCheckbox = new CheckboxInteractor('section[class*=existing] [class*="qualifier---"]');
+  existingRecordPartCheckbox = new CheckboxInteractor('section[class*=existing] [class*="part---"]');
 }
--- test/bigtest/tests/match-profiles/new-match-profiles-test.js
+++ test/bigtest/tests/match-profiles/new-match-profiles-test.js
@@ -96,22 +96,104 @@ describe('Match profile form', () => {
           expect(matchProfileForm.matchCriteria.isPresent).to.be.true;
         });
 
-        describe('"MARC field in incoming record" section', () => {
-          describe('when not filled', () => {
-            it('should have empty "Field" input', () => {
-              expect(matchProfileForm.matchCriteria.inputMain.val).to.be.equal('');
+        it('is open by default', () => {
+          expect(matchProfileForm.matchCriteria.isOpen).to.be.true;
+        });
+
+        describe('"Incoming MARC record" section', () => {
+          it('has correct label', () => {
+            expect(matchProfileForm.matchCriteria.incomingMarcRecord.label).to.be.equal('Incoming MARC record');
+          });
+
+          it('has correct length of sections', () => {
+            expect(matchProfileForm.matchCriteria.incomingMarcRecordSections.children().length).to.be.equal(3);
+          });
+
+          describe('"MARC field in incoming record" section', () => {
+            it('has correct label', () => {
+              expect(matchProfileForm.matchCriteria.incomingMarcRecordSections.children(0).label).to.be.equal('MARC field in incoming record');
             });
 
-            it('should have empty "In.1" input', () => {
-              expect(matchProfileForm.matchCriteria.inputIn1.val).to.be.equal('');
+            it('content is visible', () => {
+              expect(matchProfileForm.matchCriteria.incomingMarcRecordSections.children(0).hasContent).to.be.true;
             });
 
-            it('should have empty "In.2" input', () => {
-              expect(matchProfileForm.matchCriteria.inputIn2.val).to.be.equal('');
+            it('is optional', () => {
+              expect(matchProfileForm.matchCriteria.incomingMarcRecordSections.children(0).hasCheckbox).to.be.false;
             });
 
-            it('should have empty "Subfield" input', () => {
-              expect(matchProfileForm.matchCriteria.inputSubfield.val).to.be.equal('');
+            describe('when not filled', () => {
+              it('should have empty "Field" input', () => {
+                expect(matchProfileForm.matchCriteria.inputMain.val).to.be.equal('');
+              });
+
+              it('should have empty "In.1" input', () => {
+                expect(matchProfileForm.matchCriteria.inputIn1.val).to.be.equal('');
+              });
+
+              it('should have empty "In.2" input', () => {
+                expect(matchProfileForm.matchCriteria.inputIn2.val).to.be.equal('');
+              });
+
+              it('should have empty "Subfield" input', () => {
+                expect(matchProfileForm.matchCriteria.inputSubfield.val).to.be.equal('');
+              });
+            });
+          });
+
+          describe('"Use a qualifier" section', () => {
+            it('has correct label', () => {
+              expect(matchProfileForm.matchCriteria.incomingMarcRecordSections.children(1).label).to.be.equal('Use a qualifier');
+            });
+
+            it('content is hidden', () => {
+              expect(matchProfileForm.matchCriteria.incomingMarcRecordSections.children(1).hasContent).to.be.false;
+            });
+
+            it('is optional', () => {
+              expect(matchProfileForm.matchCriteria.incomingMarcRecordSections.children(1).hasCheckbox).to.be.true;
+            });
+
+            describe('click checkbox', () => {
+              beforeEach(async () => {
+                await matchProfileForm.matchCriteria.incomingMarcRecordQualifierCheckbox.clickAndBlur();
+              });
+
+              it('checkbox is checked', () => {
+                expect(matchProfileForm.matchCriteria.incomingMarcRecordQualifierCheckbox.isChecked).to.be.true;
+              });
+
+              it('content is visible', () => {
+                expect(matchProfileForm.matchCriteria.incomingMarcRecordSections.children(1).hasContent).to.be.true;
+              });
+            });
+          });
+
+          describe('"Only compare part of the value"', () => {
+            it('has correct label', () => {
+              expect(matchProfileForm.matchCriteria.incomingMarcRecordSections.children(2).label).to.be.equal('Only compare part of the value');
+            });
+
+            it('content is hidden', () => {
+              expect(matchProfileForm.matchCriteria.incomingMarcRecordSections.children(2).hasContent).to.be.false;
+            });
+
+            it('is optional', () => {
+              expect(matchProfileForm.matchCriteria.incomingMarcRecordSections.children(2).hasCheckbox).to.be.true;
+            });
+
+            describe('click checkbox', () => {
+              beforeEach(async () => {
+                await matchProfileForm.matchCriteria.incomingMarcRecordPartCheckbox.clickAndBlur();
+              });
+
+              it('checkbox is checked', () => {
+                expect(matchProfileForm.matchCriteria.incomingMarcRecordPartCheckbox.isChecked).to.be.true;
+              });
+
+              it('content is visible', () => {
+                expect(matchProfileForm.matchCriteria.incomingMarcRecordSections.children(2).hasContent).to.be.true;
+              });
             });
           });
         });
@@ -121,8 +203,84 @@ describe('Match profile form', () => {
             expect(matchProfileForm.matchCriteria.matchCriterionSection.isPresent).to.be.true;
           });
 
+          it('has correct label', () => {
+            expect(matchProfileForm.matchCriteria.matchCriterion.label).to.be.equal('Match criterion');
+          });
+
           it('should have a dropdown', () => {
             expect(matchProfileForm.matchCriteria.matchCriterionSection.dropdown.isPresent).to.be.true;
+          });
+        });
+
+        describe('"Existing record" section', () => {
+          it('has correct label', () => {
+            expect(matchProfileForm.matchCriteria.existingRecord.label).to.be.equal('Existing record');
+          });
+
+          it('has correct length of sections', () => {
+            expect(matchProfileForm.matchCriteria.existingRecordSections.children().length).to.be.equal(3);
+          });
+
+          describe('"Existing record field" section', () => {
+            it('has correct label', () => {
+              expect(matchProfileForm.matchCriteria.existingRecordSections.children(0).label).to.be.equal('Existing record field');
+            });
+          });
+
+          describe('"Use a qualifier section" section', () => {
+            it('has correct label', () => {
+              expect(matchProfileForm.matchCriteria.existingRecordSections.children(1).label).to.be.equal('Use a qualifier');
+            });
+
+            it('is optional', () => {
+              expect(matchProfileForm.matchCriteria.existingRecordSections.children(1).hasCheckbox).to.be.true;
+            });
+
+            it('content is hidden', () => {
+              expect(matchProfileForm.matchCriteria.existingRecordSections.children(1).hasContent).to.be.false;
+            });
+
+            describe('click checkbox', () => {
+              beforeEach(async () => {
+                await matchProfileForm.matchCriteria.existingRecordQualifierCheckbox.clickAndBlur();
+              });
+
+              it('checkbox is checked', () => {
+                expect(matchProfileForm.matchCriteria.existingRecordQualifierCheckbox.isChecked).to.be.true;
+              });
+
+              it('content is visible', () => {
+                expect(matchProfileForm.matchCriteria.existingRecordSections.children(1).hasContent).to.be.true;
+              });
+            });
+          });
+
+          describe('"Only compare part of the value"', () => {
+            it('has correct label', () => {
+              expect(matchProfileForm.matchCriteria.existingRecordSections.children(2).label).to.be.equal('Only compare part of the value');
+            });
+
+            it('is optional', () => {
+              expect(matchProfileForm.matchCriteria.existingRecordSections.children(2).hasCheckbox).to.be.true;
+            });
+
+            it('content is hidden', () => {
+              expect(matchProfileForm.matchCriteria.existingRecordSections.children(2).hasContent).to.be.false;
+            });
+
+            describe('click checkbox', () => {
+              beforeEach(async () => {
+                await matchProfileForm.matchCriteria.existingRecordPartCheckbox.clickAndBlur();
+              });
+
+              it('checkbox is checked', () => {
+                expect(matchProfileForm.matchCriteria.existingRecordPartCheckbox.isChecked).to.be.true;
+              });
+
+              it('content is visible', () => {
+                expect(matchProfileForm.matchCriteria.existingRecordSections.children(2).hasContent).to.be.true;
+              });
+            });
           });
         });
       });
