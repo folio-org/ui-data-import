931fe8724318ae4d69e02123c3f188bdd52bf956
Ihor Koba <ihor_koba@epam.com>
2020-03-17 15:39:17 +0200

    UIDATIMP-398: Remove extra action buttons in File extension Settings screens after central component update (#467)

--- test/bigtest/interactors/file-extensions/file-extensions.js
+++ test/bigtest/interactors/file-extensions/file-extensions.js
@@ -1,27 +1,19 @@
-import {
-  interactor,
-  scoped,
-} from '@bigtest/interactor';
+import { interactor } from '@bigtest/interactor';
 
 import ButtonInteractor from '@folio/stripes-components/lib/Button/tests/interactor';
 import CalloutInteractor from '@folio/stripes-components/lib/Callout/tests/interactor';
 import MultiColumnListInteractor from '@folio/stripes-components/lib/MultiColumnList/tests/interactor';
 import ConfirmationModalInteractor from '@folio/stripes-components/lib/ConfirmationModal/tests/interactor';
 import TextFieldInteractor from '@folio/stripes-components/lib/TextField/tests/interactor';
+import { ActionMenuInteractor } from '../action-menu-interactor';
 
 @interactor class FileExtensionsInteractor {
-  paneHeaderDropdown = scoped('[class*="paneHeaderCenterButton"]');
-  restoreDefaultFileExtensionsButton = new ButtonInteractor('[data-test-restore-default-file-extensions-button]');
-  newFileExtensionButton = new ButtonInteractor('[data-test-new-button]');
+  actionMenu = new ActionMenuInteractor();
   list = new MultiColumnListInteractor('#file-extensions-list');
   confirmationModal = new ConfirmationModalInteractor('#restore-default-records-modal');
   callout = new CalloutInteractor();
   searchFiled = new TextFieldInteractor('#input-file-extensions-search');
   searchSubmitButton = new ButtonInteractor('[data-test-search-and-sort-submit]');
-
-  expandPaneHeaderDropdown() {
-    return this.paneHeaderDropdown.click();
-  }
 }
 
 export const fileExtensions = new FileExtensionsInteractor();
