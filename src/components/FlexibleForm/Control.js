550bee921ba9ac7d3b55a6529945518c648e74c2
Mariia Aloshyna <mariia_aloshyna@epam.com>
2020-05-29 16:11:10 +0300

    UIDATIMP-519: Add acceptedValues obj to fields

--- src/components/FlexibleForm/Control.js
+++ src/components/FlexibleForm/Control.js
@@ -1,5 +1,6 @@
 import React, {
   Fragment,
+  useCallback,
   memo,
 } from 'react';
 import { FormattedMessage } from 'react-intl';
@@ -145,7 +146,7 @@ export const Control = memo(props => {
       attrs = {
         ...attrs,
         name: fullName,
-        fieldsPath: fullFieldsPath,
+        fieldsPath: getActualParam(fullFieldsPath, sectionNamespace, repeatableIndex),
       };
     }
 
@@ -214,6 +215,7 @@ export const Control = memo(props => {
         ...attrs,
         component: wrapper || control,
         WrappedComponent: wrapper ? control : null,
+        setAcceptedValues: data => setReferenceTables(attrs.fieldsPath, data),
       };
     }
 
@@ -434,12 +436,12 @@ export const Control = memo(props => {
         onRemove={onRemove}
         canAdd={canAdd}
         canRemove={canRemove}
-        renderField={() => (
+        renderField={(field, index) => (
           <>
             {children.map((cfg, i) => (
               <Control
                 key={`control-${i}`}
-                repeatableIndex={i}
+                repeatableIndex={index}
                 staticNamespace={staticNamespace}
                 editableNamespace={editableNamespace}
                 sectionNamespace={sectionNamespace}
