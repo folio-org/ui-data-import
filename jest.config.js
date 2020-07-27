ae6c0f3b8b3d2a03681711ef50c2f2a0793f6a59
Ivan Kryzhanovskyi <Ivan_Kryzhanovskyi@epam.com>
2020-07-27 21:08:32 +0300

    UIDATIMP-586 reverting config changes

--- jest.config.js
+++ jest.config.js
@@ -8,7 +8,7 @@ module.exports = {
     '!**/node_modules/**',
     '!**/test/**',
   ],
-  coverageDirectory: './coverage/',
+  coverageDirectory: './artifacts/coverage/',
   coverageReporters: ['lcov'],
   reporters: ['jest-junit', 'default'],
   transform: { '^.+\\.(js|jsx)$': path.join(__dirname, './test/jest/jest-transformer.js') },
