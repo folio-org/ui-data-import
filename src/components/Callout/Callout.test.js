76a95bd0d9b8a2528587fff7176eba39475a673c
Ihor Koba <ihor_koba@epam.com>
2020-11-23 16:24:29 +0200

    UIDATIMP-701: Cover Callout component with tests

---
+++ src/components/Callout/Callout.test.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import '../../../test/jest/__mock__';
+
+import { createNetworkMessage } from './Callout';
+
+const mockCalloutRef = {
+  current: {
+    sendCallout: jest.fn(() => <span>Message</span>),
+  },
+}
+
+const inputParams = ['success', 'fileExtensions', mockCalloutRef];
+const callBackFn = createNetworkMessage(...inputParams);
+
+describe('createNetworkMessage', () => {
+  afterEach(() => {
+    mockCalloutRef.current.sendCallout.mockClear();
+  });
+
+  describe('when function is called', () => {
+    it('then it should send message component through call of the `sendCallout`', async() => {
+      await callBackFn('create', { name: 'test record name'}, {})
+
+      expect(mockCalloutRef.current.sendCallout).toBeCalledTimes(1);
+    });
+  });
+});
