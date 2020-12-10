ae6c7e52c6177dd4df61ea6ff1c4db4b0a5c3308
Mariia Aloshyna <55138456+mariia-aloshyna@users.noreply.github.com>
2020-12-10 17:43:36 +0200

    UIDATIMP-671: Add filter for single record imports to the View all screen (#783)

--- src/routes/ViewAllLogs/ViewAllLogsFilterConfig.js
+++ src/routes/ViewAllLogs/ViewAllLogsFilterConfig.js
@@ -2,6 +2,10 @@ import {
   FILTERS,
   LOGS_FILTER,
 } from './constants';
+import {
+  OCLC_CREATE_INSTANCE_ID,
+  OCLC_UPDATE_INSTANCE_ID,
+} from '../../utils';
 
 export const filterConfig = [
   {
@@ -25,5 +29,21 @@ export const filterConfig = [
     name: FILTERS.USER,
     cql: `${LOGS_FILTER} AND ${FILTERS.USER}`,
     values: [],
+  },
+  {
+    name: FILTERS.SINGLE_RECORD_IMPORTS,
+    cql: FILTERS.JOB_PROFILE,
+    operator: '=',
+    values: [
+      {
+        name: 'no',
+        cql: `\\“id\\“==" NOT jobProfileInfo="\\“id\\“=="${OCLC_CREATE_INSTANCE_ID}") 
+        AND (jobProfileInfo="\\“id\\“==" NOT jobProfileInfo="\\“id\\“=="${OCLC_UPDATE_INSTANCE_ID}"`,
+      },
+      {
+        name: 'yes',
+        cql: `\\“id\\“==${OCLC_CREATE_INSTANCE_ID}") OR (jobProfileInfo="\\“id\\“==${OCLC_UPDATE_INSTANCE_ID}`,
+      },
+    ],
   },
 ];
--- src/routes/ViewAllLogs/constants.js
+++ src/routes/ViewAllLogs/constants.js
@@ -1,4 +1,7 @@
-import { FILE_STATUSES } from '../../utils/constants';
+import React from 'react';
+import { FormattedMessage } from 'react-intl';
+
+import { FILE_STATUSES } from '../../utils';
 
 const {
   COMMITTED,
@@ -10,17 +13,28 @@ export const FILTERS = {
   DATE: 'completedDate',
   JOB_PROFILE: 'jobProfileInfo',
   USER: 'userId',
+  SINGLE_RECORD_IMPORTS: 'singleRecordImports',
 };
 
 export const FILTER_OPTIONS = {
   ERRORS: [
     {
       value: COMMITTED,
-      label: 'No',
+      label: <FormattedMessage id="ui-data-import.filter.option.no" />,
     },
     {
       value: ERROR,
-      label: 'Yes',
+      label: <FormattedMessage id="ui-data-import.filter.option.yes" />,
+    },
+  ],
+  SINGLE_RECORD_IMPORTS: [
+    {
+      value: 'no',
+      label: <FormattedMessage id="ui-data-import.filter.option.no" />,
+    },
+    {
+      value: 'yes',
+      label: <FormattedMessage id="ui-data-import.filter.option.yes" />,
     },
   ],
 };
