df578ea01363f83670c68b432b44e9a793bed2dc
Oleksandr Hladchenko1 <oleksandr_hladchenko1@epam.com>
2021-06-08 16:52:13 +0300

    UIDATIMP-724: Add imports

---
+++ src/components/Section/Section.test.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+import { renderWithIntl } from '@folio/stripes-data-transfer-components/test/jest/helpers';
+
+import '../../../test/jest/__mock__';
+import { translationsProperties } from '../../../test/jest/helpers';
+
+import { Section } from './Section';
