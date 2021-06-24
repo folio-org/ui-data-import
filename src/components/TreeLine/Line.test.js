e26d679e76b6c21675c5949e4bc0767ee37fd27f
OleksandrHladchenko1 <85172747+OleksandrHladchenko1@users.noreply.github.com>
2021-06-24 11:28:58 +0300

    UIDATIMP-727: Cover <TreeLine> component with tests (#925)
    * UIDATIMP-727: 100% coverage with tests
    
    * UIDATIMP-727: Fixed Jenkins issues
    
    * UIDATIMP-727: Fixed Jenkins issues final
    
    * UIDATIMP-727: Fixed review issues
    
    * UIDATIMP-727: Fixed review issues and mocked ResizeObserver
    
    * UIDATIMP-727: Fixed final review issues
    
    Co-authored-by: Ivan Kryzhanovskyi <63101175+IvanKryzhanovskyi@users.noreply.github.com>

---
+++ src/components/TreeLine/Line.test.js
@@ -0,0 +1,61 @@
+import React from 'react';
+
+import { render } from '@testing-library/react';
+
+import '../../../test/jest/__mock__';
+
+import { Line } from './Line';
+
+const parentContainer = <div>{' '}</div>;
+
+const lineWithParentContainer = {
+  x0: 0,
+  y0: 0,
+  x1: 0,
+  y1: 10,
+  container: parentContainer,
+  className: 'tester',
+  borderWidth: 2,
+  borderStyle: 'solid',
+  borderColor: '#000',
+  zIndex: 2,
+};
+
+const renderLine = ({
+  x0,
+  y0,
+  x1,
+  y1,
+  container,
+  className,
+  borderWidth,
+  borderStyle,
+  borderColor,
+  zIndex,
+}) => {
+  const component = (
+    <Line
+      x0={x0}
+      y0={y0}
+      x1={x1}
+      y1={y1}
+      container={container}
+      className={className}
+      borderWidth={borderWidth}
+      borderStyle={borderStyle}
+      borderColor={borderColor}
+      zIndex={zIndex}
+    />
+  );
+
+  return render(component);
+};
+
+describe('Line', () => {
+  it('should be rendered in parent element', () => {
+    const { container } = renderLine(lineWithParentContainer);
+    const line = container.querySelector('.tester');
+
+    expect(line).toBeDefined();
+  });
+});
---
+++ src/components/TreeLine/TreeLine.test.js
@@ -0,0 +1,155 @@
+import React from 'react';
+
+import { render } from '@testing-library/react';
+
+import '../../../test/jest/__mock__';
+
+import { TreeLine } from './TreeLine';
+
+const parentContainer = (
+  <div
+    id="test"
+    className="tester"
+  >
+    <div data-id="HOLDINGS">testFrom</div>
+    <div data-id="INSTANCE">testTo</div>
+  </div>
+);
+const treeLineWithContainer = {
+  from: '[data-id=HOLDINGS]',
+  to: '[data-id=INSTANCE]',
+  container: '#test',
+  fromAnchor: '12px 12px',
+  toAnchor: '13px 13px',
+  isLocalLTR: false,
+  orientation: 'horizontal',
+  fromAnchorOffset: '10px 10px',
+  toAnchorOffset: '13px 13px',
+  className: 'test',
+};
+const treeLineHorizontal = {
+  from: '[data-id=HOLDINGS]',
+  to: '[data-id=INSTANCE]',
+  container: '#test',
+  fromAnchor: '12%',
+  toAnchor: '13%',
+  isLocalLTR: true,
+  orientation: 'horizontal',
+  fromAnchorOffset: '10px 10px',
+  toAnchorOffset: '13px 13px',
+  className: 'horizontal',
+};
+const treeLineVertical = {
+  from: '[data-id=HOLDINGS]',
+  to: '[data-id=INSTANCE]',
+  container: '#test',
+  fromAnchor: 'left',
+  toAnchor: 'left',
+  isLocalLTR: false,
+  orientation: 'vertical',
+  fromAnchorOffset: '10px 10px',
+  toAnchorOffset: '13px 13px',
+  className: 'vertical',
+};
+const oneTreeLine = {
+  from: '[data-id=HOLDINGS]',
+  to: '[data-id=INSTANCE]',
+  container: '#test',
+  fromAnchor: 'left',
+  toAnchor: 'left',
+  fromAnchorOffset: '10px 10px',
+  toAnchorOffset: '10px 10px',
+  className: 'one-line',
+};
+const treeLineWithoutContainer = {
+  from: '[data-id=HOLDINGS]',
+  to: '[data-id=INSTANCE]',
+  container: null,
+  fromAnchor: '12px 12px',
+  toAnchor: '13px 13px',
+  className: 'test',
+};
+
+const renderTreeLine = ({
+  from,
+  to,
+  container,
+  fromAnchor,
+  toAnchor,
+  isLocalLTR,
+  orientation,
+  fromAnchorOffset,
+  toAnchorOffset,
+  className,
+}) => {
+  const component = (
+    <TreeLine
+      from={from}
+      to={to}
+      container={container}
+      fromAnchor={fromAnchor}
+      toAnchor={toAnchor}
+      isLocalLTR={isLocalLTR}
+      orientation={orientation}
+      fromAnchorOffset={fromAnchorOffset}
+      toAnchorOffset={toAnchorOffset}
+      className={className}
+    />
+  );
+
+  return render(component);
+};
+
+window.ResizeObserver = jest.fn(() => ({
+  observe() {},
+  unobserve() {},
+}));
+
+describe('TreeLine', () => {
+  afterAll(() => {
+    delete window.ResizeObserver;
+  });
+
+  it('should be rendered inside container', () => {
+    const { container } = render(parentContainer);
+
+    renderTreeLine(treeLineWithContainer);
+    const element = container.querySelector('.test');
+
+    expect(element).toBeDefined();
+  });
+
+  it('should be rendered inside document.body when container is not defined', () => {
+    const { container } = renderTreeLine(treeLineWithoutContainer);
+    const element = container.querySelector('.test');
+
+    expect(element).toBeDefined();
+  });
+
+  it('should be rendered vertically', () => {
+    const { container } = render(parentContainer);
+
+    renderTreeLine(treeLineVertical);
+    const element = container.querySelector('.vertical');
+
+    expect(element).toBeDefined();
+  });
+
+  it('should be rendered horizontally', () => {
+    const { container } = render(parentContainer);
+
+    renderTreeLine(treeLineHorizontal);
+    const element = container.querySelector('.horizontal');
+
+    expect(element).toBeDefined();
+  });
+
+  it('should be rendered one line', () => {
+    const { container } = render(parentContainer);
+
+    renderTreeLine(oneTreeLine);
+    const element = container.querySelector('.one-line');
+
+    expect(element).toBeDefined();
+  });
+});
