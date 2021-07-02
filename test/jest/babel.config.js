bea1cee13eb67284199e5ef6c5b011daa9cf47ff
Mariia Aloshyna <Mariia_Aloshyna@epam.com>
2021-07-02 12:50:43 +0300

    UIDATIMP-733: Cover `<WithTranslation>` component with unit tests

--- test/jest/babel.config.js
+++ test/jest/babel.config.js
@@ -6,6 +6,7 @@ module.exports = {
   plugins: [
     ['@babel/plugin-proposal-decorators', { legacy: true }],
     ['@babel/plugin-proposal-class-properties', { loose: true }],
+    ['@babel/plugin-proposal-private-methods', { loose: true }],
     '@babel/plugin-transform-runtime',
   ],
 };
