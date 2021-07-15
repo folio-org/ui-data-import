576ec867a236ad6b23c254e9262d791902a798a8
Ivan Kryzhanovskyi <Ivan_Kryzhanovskyi@epam.com>
2021-07-15 16:08:54 +0300

    UIDATIMP-950 fix according comment

--- src/components/DataFetcher/DataFetcher.js
+++ src/components/DataFetcher/DataFetcher.js
@@ -13,6 +13,8 @@ import { jobExecutionPropTypes } from '../Jobs/components/Job/jobExecutionPropTy
 import {
   JOB_STATUSES,
   FILE_STATUSES,
+  OCLC_CREATE_INSTANCE_JOB_ID,
+  OCLC_UPDATE_INSTANCE_JOB_ID,
 } from '../../utils';
 
 import { DataFetcherContext } from '.';
@@ -36,8 +38,8 @@ const jobsUrl = createUrl('metadata-provider/jobExecutions', {
 
 const logsUrl = createUrl('metadata-provider/jobExecutions', {
   query: `(status any "${COMMITTED} ${ERROR}") 
-  AND (jobProfileInfo="\\“id\\“==" NOT jobProfileInfo="\\“id\\“=="d0ebb7b0-2f0f-11eb-adc1-0242ac120002") 
-  AND (jobProfileInfo="\\“id\\“==" NOT jobProfileInfo="\\“id\\“=="91f9b8d6-d80e-4727-9783-73fb53e3c786"") 
+  AND (jobProfileInfo="\\“id\\“==" NOT jobProfileInfo="\\“id\\“=="${OCLC_CREATE_INSTANCE_JOB_ID}") 
+  AND (jobProfileInfo="\\“id\\“==" NOT jobProfileInfo="\\“id\\“=="${OCLC_UPDATE_INSTANCE_JOB_ID}") 
   sortBy completedDate/sort.descending`,
   limit: 25,
 }, false);
