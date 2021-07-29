db404aa7bd4b5a58d0540e0bc56412dcd2d0072d
KYIV\Oleksandr_Hladchenko <oleksandr_hladchenko1@epam.com>
2021-07-29 16:28:41 +0300

    Fix wrong props issue

--- src/components/LogViewer/LogViewer.test.js
+++ src/components/LogViewer/LogViewer.test.js
@@ -10,31 +10,32 @@ import { LogViewer } from './LogViewer';
 const logViewerLogsProps = {
   logs: {
     0: [{
-      label: 'test label',
-      logs: ["{ name: 'test' }"],
+      label: 'srs-marc-bib label',
+      logs: [{ "id":"0", "name": "test" }],
       errorBlockId: 'srs-marc-bib-error',
     }],
     1: [{
-      label: 'test label',
-      error: jest.fn(),
+      label: 'instance label',
+      error: 'test error',
+      errorBlockId: 'instance-error',
     }],
     2: [{
-      label: 'test label',
-      logs: ["{ name: 'test' }"],
+      label: 'holdings label',
+      logs: [{ "id":"2", "name": "test" }],
       errorBlockId: 'holdings-error',
     }],
     3: [{
-      label: 'test label',
-      logs: ["{ name: 'test' }"],
+      label: 'item label',
+      logs: [{ "id":"3", "name": "test" }],
       errorBlockId: 'item-error',
     }],
     4: [{}],
     5: [{
-      label: 'test label',
-      logs: ["{ name: 'test' }"],
+      label: 'invoice label',
+      logs: [{ "id":"5", "name": "test" }],
       errorBlockId: 'invoice-error',
     }, {
-      label: 'test label',
+      label: 'invoice-line label',
       logs: [],
       errorBlockId: 'invoice-line-error',
     }],
@@ -73,7 +74,7 @@ const renderLogViewer = ({
 describe('LogViewer', () => {
   it('should be rendered with default theme', () => {
     const { getByText } = renderLogViewer(logViewerLogsProps);
-    const preElement = getByText('test label');
+    const preElement = getByText('srs-marc-bib label');
 
     expect(preElement).toHaveClass('coy');
   });
@@ -83,7 +84,7 @@ describe('LogViewer', () => {
       ...logViewerOtherProps,
     });
 
-    expect(getByText('test label')).toBeDefined();
+    expect(getByText('srs-marc-bib label')).toBeDefined();
   });
 
   describe('when there is no data', () => {
@@ -191,16 +192,16 @@ describe('LogViewer', () => {
     it('should change value', () => {
       const {
         container,
-        getByText,
+        getByText, debug
       } = renderLogViewer({
         ...logViewerLogsProps,
         ...logViewerOtherProps,
       });
-      const preElement = getByText('test label');
+      const preElement = getByText('srs-marc-bib label');
       const selectControl = container.querySelector('.selectControl');
 
       fireEvent.change(selectControl, { target: { value: 'coy' } });
-
+debug();
       expect(preElement).toHaveClass('coy');
     });
   });
