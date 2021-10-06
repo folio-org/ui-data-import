65ebae5af7cb3b1b29032f0966766783aa19364e
javokhir12 <javokhir_rajabboev@epam.com>
2021-10-06 18:46:49 +0500

    UIDATIMP-957: Cover <EditKeyShortcutsWrapper> component with unit tests

---
+++ src/components/EditKeyShortcutsWrapper/EditKeyShortcutsWrapper.test.js
@@ -0,0 +1,60 @@
+import React from 'react';
+
+import { renderWithIntl } from '@folio/stripes-data-transfer-components/test/jest/helpers';
+
+import '../../../test/jest/__mock__';
+
+import { waitFor } from '@testing-library/react';
+
+import {
+  renderWithReduxForm,
+  translationsProperties,
+  saveShortcut,
+} from '../../../test/jest/helpers';
+
+import { EditKeyShortcutsWrapper } from './EditKeyShortcutsWrapper';
+
+const {
+  CommandList,
+  defaultKeyboardShortcuts,
+} = require('@folio/stripes/components');
+
+const mockOnSubmitProp = jest.fn(async () => 'test value');
+
+jest.mock('@folio/stripes/smart-components', () => ({ ...jest.requireActual('@folio/stripes/smart-components') }), { virtual: true });
+
+const renderEditKeyShortcutsWrapper = ({ onSubmit }) => {
+  const childElement = <span>Child element</span>;
+
+  const component = () => (
+    <CommandList commands={defaultKeyboardShortcuts}>
+      <EditKeyShortcutsWrapper
+        onSubmit={onSubmit}
+      >
+        {childElement}
+      </EditKeyShortcutsWrapper>
+    </CommandList>
+  );
+
+  return renderWithIntl(renderWithReduxForm(component), translationsProperties);
+};
+
+describe('EditKeyShortcutsWrapper component', () => {
+  it('should render children correctly', () => {
+    const { getByText } = renderEditKeyShortcutsWrapper({ onSubmit: mockOnSubmitProp });
+
+    expect(getByText('Child element')).toBeInTheDocument();
+  });
+
+  it('should call the assigned handler to (save) command', async () => {
+    const { getByText } = renderEditKeyShortcutsWrapper({ onSubmit: mockOnSubmitProp });
+
+    const childElement = getByText('Child element');
+
+    childElement.focus();
+
+    saveShortcut(childElement);
+
+    await waitFor(() => expect(mockOnSubmitProp).toHaveBeenCalled());
+  });
+});
