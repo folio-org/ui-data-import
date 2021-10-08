ea377b78dc3ea4f97046e75421eb6fa88a447ad0
javokhir12 <javokhir_rajabboev@epam.com>
2021-10-08 18:45:35 +0500

    UIDATIMP-960: Refactor ReturnToAssignJobs.test.js file

--- src/components/ImportJobs/components/ReturnToAssignJobs/ReturnToAssignJobs.test.js
+++ src/components/ImportJobs/components/ReturnToAssignJobs/ReturnToAssignJobs.test.js
@@ -1,23 +1,21 @@
 import React from 'react';
-
+import { fireEvent } from '@testing-library/react';
 import { noop } from 'lodash';
 
 import { renderWithIntl } from '@folio/stripes-data-transfer-components/test/jest/helpers';
 
 import '../../../../../test/jest/__mock__';
 
-import { fireEvent } from '@testing-library/react';
-
-import { UploadingJobsContext } from '../../../UploadingJobsContextProvider';
+import { FILE_STATUSES } from '../../../../utils';
 
 import {
-  renderWithReduxForm,
+  renderWithFinalForm,
   translationsProperties,
 } from '../../../../../test/jest/helpers';
 
-import { ReturnToAssignJobs } from './ReturnToAssignJobs';
+import { UploadingJobsContext } from '../../../UploadingJobsContextProvider';
 
-import { FILE_STATUSES } from '../../../../utils';
+import { ReturnToAssignJobs } from './ReturnToAssignJobs';
 
 jest.mock('@folio/stripes-data-transfer-components', () => ({ Preloader: () => <span>Preloader</span> }));
 
@@ -38,25 +36,17 @@ const defaultContext = {
   deleteUploadDefinition: mockDeleteUploadDefinition,
 };
 
-const returnToAssignJobsProps = {
-  onResume: mockOnResumeProp,
-  prohibitFilesUploading: false,
-};
-
-const renderReturnToAssignJobs = ({
-  onResume,
-  prohibitFilesUploading,
-}) => {
+const renderReturnToAssignJobs = () => {
   const component = () => (
     <UploadingJobsContext.Provider value={defaultContext}>
       <ReturnToAssignJobs
-        onResume={onResume}
-        prohibitFilesUploading={prohibitFilesUploading}
+        onResume={mockOnResumeProp}
+        prohibitFilesUploading={false}
       />
     </UploadingJobsContext.Provider>
   );
 
-  return renderWithIntl(renderWithReduxForm(component), translationsProperties);
+  return renderWithIntl(renderWithFinalForm(component), translationsProperties);
 };
 
 describe('ReturnToAssignJobs component', () => {
@@ -67,23 +57,19 @@ describe('ReturnToAssignJobs component', () => {
 
   it('should render correctly', () => {
     const {
-      getByText,
-      getByRole,
-    } = renderReturnToAssignJobs(returnToAssignJobsProps);
+      getByText, getByRole,
+    } = renderReturnToAssignJobs();
 
     const resumeButton = getByRole('button', { name: /resume/i });
-
     const deleteFilesButton = getByText(/delete files/i);
 
     expect(resumeButton).toBeInTheDocument();
-
     expect(deleteFilesButton).toBeInTheDocument();
-
     expect(getByText(/1 file uploaded/i)).toBeInTheDocument();
   });
 
   it('should call the assigned handler when Resume button clicked', () => {
-    const { getByRole } = renderReturnToAssignJobs(returnToAssignJobsProps);
+    const { getByRole } = renderReturnToAssignJobs();
 
     const resumeButton = getByRole('button', { name: /resume/i });
 
@@ -92,30 +78,29 @@ describe('ReturnToAssignJobs component', () => {
     expect(mockOnResumeProp).toHaveBeenCalled();
   });
 
-  it('should render Preloader when delete files button clicked', async () => {
-    const { getByText } = renderReturnToAssignJobs(returnToAssignJobsProps);
+  it('should render Preloader when delete files button clicked', () => {
+    const { getByText } = renderReturnToAssignJobs();
 
     const deleteFilesButton = getByText(/delete files/i);
 
     fireEvent.click(deleteFilesButton);
 
-    expect(getByText(/preloader/i)).toBeInTheDocument();
+    expect(getByText('Preloader')).toBeInTheDocument();
   });
 
   it('should still show Preloader when delete button clicked multiple times ', () => {
-    const { getByText } = renderReturnToAssignJobs(returnToAssignJobsProps);
+    const { getByText } = renderReturnToAssignJobs();
 
     const deleteFilesButton = getByText(/delete files/i);
 
     fireEvent.click(deleteFilesButton);
-
     fireEvent.click(deleteFilesButton);
 
-    expect(getByText(/preloader/i)).toBeInTheDocument();
+    expect(getByText('Preloader')).toBeInTheDocument();
   });
 
   it('should handle deletion error correctly', () => {
-    const { getByText } = renderReturnToAssignJobs(returnToAssignJobsProps);
+    const { getByText } = renderReturnToAssignJobs();
 
     const deleteFilesButton = getByText(/delete files/i);
 
