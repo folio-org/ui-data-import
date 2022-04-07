8c9c14e6e03769579e55ad92457492bbdef46d61
Oleksandr Hladchenko1 <oleksandr_hladchenko1@epam.com>
2022-04-07 12:27:02 +0300

    Edit linter issues

--- test/bigtest/.eslintrc.js
+++ test/bigtest/.eslintrc.js
@@ -1,8 +1,8 @@
 module.exports = {
   rules: {
-    "func-names": "off",
-    "no-unused-expressions": "off",
-    "one-var": "off",
-    "import/prefer-default-export": "off"
+    'func-names': 'off',
+    'no-unused-expressions': 'off',
+    'one-var': 'off',
+    'import/prefer-default-export': 'off'
   }
 };
