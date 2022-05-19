0d4c9526f4c5edd84b23cef58d268f16b8cbcb06
Javokhir Rajabboev <javokhir_rajabboev@epam.com>
2022-05-19 14:14:10 +0500

    UIDATIMP-1174: Cover showActionMenu with test

---
+++ src/utils/tests/showActionMenu.test.js
@@ -0,0 +1,22 @@
+import { showActionMenu } from '../showActionMenu';
+import { permissions } from '../permissions';
+
+describe('showActionMenu function', () => {
+  const renderer = jest.fn();
+
+  it('should render action menu when user has correct permission', () => {
+    const stripes = {
+      hasPerm: perm => perm === permissions.SETTINGS_MANAGE
+    };
+
+    expect(showActionMenu({ renderer, stripes })).toEqual(renderer);
+  });
+
+  it('should return null when user does not have correct permission', () => {
+    const stripes = {
+      hasPerm: perm => perm === 'test.permission'
+    };
+
+    expect(showActionMenu({ renderer, stripes })).toBeNull();
+  });
+});
