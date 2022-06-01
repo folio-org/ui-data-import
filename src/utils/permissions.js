e8da9f20086ce05c352f68971c95b21aa3433788
Oleksandr Hladchenko1 <oleksandr_hladchenko1@epam.com>
2022-06-01 17:19:00 +0300

    UIDATIMP-1143: Add permissions

--- src/utils/permissions.js
+++ src/utils/permissions.js
@@ -1,4 +1,6 @@
 export const permissions = {
   SETTINGS_MANAGE: 'ui-data-import.settings.manage',
+  SETTINGS_VIEW_ONLY: 'ui-data-import.settings.readOnly',
+  DATA_IMPORT_MANAGE: 'ui-data-import.manage',
   DELETE_LOGS: 'ui-data-import.logs.delete',
 };
