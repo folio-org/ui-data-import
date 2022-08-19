1d8308a36b4ad9b96d8a39137138d9a2b7b84168
Mariia Aloshyna <55138456+mariia-aloshyna@users.noreply.github.com>
2022-08-19 11:06:59 +0300

    UIDATIMP-1239: Get rid of jobExecutions request for the Preview section (#1207)

--- src/components/Jobs/components/Job/jobMetaTypes.js
+++ src/components/Jobs/components/Job/jobMetaTypes.js
@@ -1,18 +1,6 @@
 import { JOB_STATUSES } from '../../../../utils/constants';
 
 export const jobMetaTypes = {
-  [JOB_STATUSES.PREPARING_FOR_PREVIEW]: job => ({
-    showProgress: true,
-    showPreview: false,
-    date: job.startedDate,
-    dateLabel: 'began',
-  }),
-  [JOB_STATUSES.READY_FOR_PREVIEW]: job => ({
-    showProgress: false,
-    showPreview: true,
-    date: job.completedDate,
-    dateLabel: 'ended',
-  }),
   [JOB_STATUSES.RUNNING]: job => ({
     showProgress: true,
     showPreview: false,
