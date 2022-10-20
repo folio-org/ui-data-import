b791f52f957bfeaed1299c08e3fbc5a4e554d93b
Mariia Aloshyna <Mariia_Aloshyna@epam.com>
2022-10-20 11:13:47 +0300

    UIDATIMP-1240: Fix job profiles saving

--- src/settings/JobProfiles/JobProfilesForm.js
+++ src/settings/JobProfiles/JobProfilesForm.js
@@ -98,7 +98,8 @@ export const JobProfilesFormComponent = memo(({
   onCancel,
   stripes,
   parentResources,
-  onSubmitSuccess,
+  transitionToParams,
+  baseUrl,
   accordionStatusRef,
   layerType,
   mutator,
@@ -242,7 +243,8 @@ export const JobProfilesFormComponent = memo(({
       if (record) {
         clearStorage();
 
-        onSubmitSuccess(record, form.reset);
+        form.reset();
+        transitionToParams({ _path: `${baseUrl}/view/${record.id}` });
       }
     }
   };
@@ -384,7 +386,8 @@ JobProfilesFormComponent.propTypes = {
   onCancel: PropTypes.func.isRequired,
   stripes: PropTypes.object.isRequired,
   parentResources: PropTypes.object.isRequired,
-  onSubmitSuccess: PropTypes.func.isRequired,
+  transitionToParams: PropTypes.func.isRequired,
+  baseUrl: PropTypes.string.isRequired,
   mutator: PropTypes.shape({ childWrappers: PropTypes.shape({ GET: PropTypes.func }) }).isRequired,
   resources: PropTypes.shape({ childWrappers: PropTypes.shape({ records: PropTypes.arrayOf(PropTypes.object) }) }).isRequired,
   accordionStatusRef: PropTypes.object,
