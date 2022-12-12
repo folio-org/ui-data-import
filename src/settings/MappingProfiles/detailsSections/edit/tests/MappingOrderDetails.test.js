0bedd2cfb63cab557eb4709d8f345eeaf890ffac
Mariia Aloshyna <55138456+mariia-aloshyna@users.noreply.github.com>
2022-12-12 02:45:05 +0200

    UIDATIMP-1291: Cover Order field mapping profile with unit tests (#1295)

---
+++ src/settings/MappingProfiles/detailsSections/edit/tests/MappingOrderDetails.test.js
@@ -0,0 +1,99 @@
+import React from 'react';
+
+import { renderWithIntl } from '@folio/stripes-data-transfer-components/test/jest/helpers';
+
+import '../../../../../../test/jest/__mock__';
+import {
+  renderWithReduxForm,
+  translationsProperties,
+} from '../../../../../../test/jest/helpers';
+
+import { MappingOrderDetails } from '../MappingOrderDetails';
+import {
+  getInitialDetails,
+  getInitialFields,
+} from '../../../initialDetails';
+import { FOLIO_RECORD_TYPES } from '../../../../../components';
+
+const initialFieldsProp = getInitialFields(FOLIO_RECORD_TYPES.ORDER.type);
+const mappingDetailsProp = getInitialDetails(FOLIO_RECORD_TYPES.ORDER.type);
+const referenceTablesProp = {
+  notes: [initialFieldsProp.notes],
+  contributors: [initialFieldsProp.contributors],
+  productIds: [initialFieldsProp.productIds],
+  vendorDetail: [initialFieldsProp.vendorDetail],
+  fundDistribution: [initialFieldsProp.fundDistribution],
+  locations: [initialFieldsProp.locations],
+  volumes: [initialFieldsProp.volumes],
+};
+const setReferenceTablesMockProp = jest.fn();
+const okapiProp = {
+  tenant: 'testTenant',
+  token: 'token.for.test',
+  url: 'https://folio-testing-okapi.dev.folio.org',
+};
+
+const renderMappingOrderDetails = () => {
+  const component = () => (
+    <MappingOrderDetails
+      mappingDetails={mappingDetailsProp}
+      initialFields={initialFieldsProp}
+      referenceTables={referenceTablesProp}
+      setReferenceTables={setReferenceTablesMockProp}
+      okapi={okapiProp}
+    />
+  );
+
+  return renderWithIntl(renderWithReduxForm(component), translationsProperties);
+};
+
+
+describe('MappingOrderDetails', () => {
+  it('should have correct sections', async () => {
+    const {
+      findByRole,
+      getByRole,
+    } = renderMappingOrderDetails();
+
+    expect(await findByRole('button', {
+      name: /order information/i,
+      expanded: true,
+    })).toBeInTheDocument();
+    expect(getByRole('button', {
+      name: /order line information/i,
+      expanded: true,
+    })).toBeInTheDocument();
+    expect(getByRole('button', {
+      name: /item details/i,
+      expanded: true,
+    })).toBeInTheDocument();
+    expect(getByRole('button', {
+      name: /po line details/i,
+      expanded: true,
+    })).toBeInTheDocument();
+    expect(getByRole('button', {
+      name: /vendor/i,
+      expanded: true,
+    })).toBeInTheDocument();
+    expect(getByRole('button', {
+      name: /cost details/i,
+      expanded: true,
+    })).toBeInTheDocument();
+    expect(getByRole('button', {
+      name: /fund distribution/i,
+      expanded: true,
+    })).toBeInTheDocument();
+    expect(getByRole('button', {
+      name: /location/i,
+      expanded: true,
+    })).toBeInTheDocument();
+    expect(getByRole('button', {
+      name: /physical resource details/i,
+      expanded: true,
+    })).toBeInTheDocument();
+    expect(getByRole('button', {
+      name: /e-resources details/i,
+      expanded: true,
+    })).toBeInTheDocument();
+  });
+});
---
+++ src/settings/MappingProfiles/detailsSections/view/tests/MappingOrderDetails.test.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import {
+  fireEvent,
+  waitFor
+} from '@testing-library/react';
+
+import { renderWithIntl } from '@folio/stripes-data-transfer-components/test/jest/helpers';
+
+import '../../../../../../test/jest/__mock__';
+import { translationsProperties } from '../../../../../../test/jest/helpers';
+
+import { MappingOrderDetails } from '../MappingOrderDetails';
+import ORDER from '../../../initialDetails/ORDER';
+
+const renderMappingOrderDetails = () => {
+  const component = (
+    <MappingOrderDetails mappingDetails={ORDER.mappingFields} />
+  );
+
+  return renderWithIntl(component, translationsProperties);
+};
+
+describe('MappingOrderDetails', () => {
+  it('should have correct sections', () => {
+    const { getByRole } = renderMappingOrderDetails();
+
+    expect(getByRole('button', { name: /order information/i })).toBeInTheDocument();
+    expect(getByRole('button', { name: /order line information/i })).toBeInTheDocument();
+    expect(getByRole('button', { name: /item details/i })).toBeInTheDocument();
+    expect(getByRole('button', { name: /po line details/i })).toBeInTheDocument();
+    expect(getByRole('button', { name: /vendor/i })).toBeInTheDocument();
+    expect(getByRole('button', { name: /cost details/i })).toBeInTheDocument();
+    expect(getByRole('button', { name: /fund distribution/i })).toBeInTheDocument();
+    expect(getByRole('button', { name: /location/i })).toBeInTheDocument();
+    expect(getByRole('button', { name: /physical resource details/i })).toBeInTheDocument();
+    expect(getByRole('button', { name: /e-resources details/i })).toBeInTheDocument();
+  });
+
+  it('all sections are expanded by default', () => {
+    const { getByRole } = renderMappingOrderDetails();
+
+    expect(getByRole('button', { name: /order information/i })).toHaveAttribute('aria-expanded', 'true');
+    expect(getByRole('button', { name: /order line information/i })).toHaveAttribute('aria-expanded', 'true');
+    expect(getByRole('button', { name: /item details/i })).toHaveAttribute('aria-expanded', 'true');
+    expect(getByRole('button', { name: /po line details/i })).toHaveAttribute('aria-expanded', 'true');
+    expect(getByRole('button', { name: /vendor/i })).toHaveAttribute('aria-expanded', 'true');
+    expect(getByRole('button', { name: /cost details/i })).toHaveAttribute('aria-expanded', 'true');
+    expect(getByRole('button', { name: /fund distribution/i })).toHaveAttribute('aria-expanded', 'true');
+    expect(getByRole('button', { name: /location/i })).toHaveAttribute('aria-expanded', 'true');
+    expect(getByRole('button', { name: /physical resource details/i })).toHaveAttribute('aria-expanded', 'true');
+    expect(getByRole('button', { name: /e-resources details/i })).toHaveAttribute('aria-expanded', 'true');
+  });
+});
