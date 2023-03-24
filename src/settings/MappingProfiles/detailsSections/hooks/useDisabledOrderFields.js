78fd4905551bc31930e0b2bde12c6ced08a568d1
Mariia Aloshyna <Mariia_Aloshyna@epam.com>
2023-03-24 14:27:49 +0200

    UIDATIMP-1401: Remove useMemo from hook

--- src/settings/MappingProfiles/detailsSections/hooks/useDisabledOrderFields.js
+++ src/settings/MappingProfiles/detailsSections/hooks/useDisabledOrderFields.js
@@ -1,14 +1,12 @@
-import { useMemo } from 'react';
-
 import { useFieldMappingFieldValue } from './useFieldMappingFieldValue';
 
 import {
   ORDER_FORMATS,
-  PO_STATUS
+  PO_STATUS,
 } from '../constants';
 import {
   ORDER_FORMAT_FILED,
-  PO_STATUS_FIELD
+  PO_STATUS_FIELD,
 } from '../../../../utils';
 
 /**
@@ -19,11 +17,9 @@ import {
 export const useDisabledOrderFields = () => {
   const [poStatus, orderFormat] = useFieldMappingFieldValue([PO_STATUS_FIELD, ORDER_FORMAT_FILED]);
 
-  const dismissCreateInventory = useMemo(() => poStatus === PO_STATUS.OPEN, [poStatus]);
-  const dismissPhysicalDetails = useMemo(() => orderFormat === ORDER_FORMATS.ELECTRONIC_RESOURCE, [orderFormat]);
-  const dismissElectronicDetails = useMemo(() => {
-    return (orderFormat === ORDER_FORMATS.PHYSICAL_RESOURCE) || (orderFormat === ORDER_FORMATS.OTHER);
-  }, [orderFormat]);
+  const dismissCreateInventory = poStatus === PO_STATUS.OPEN;
+  const dismissPhysicalDetails = orderFormat === ORDER_FORMATS.ELECTRONIC_RESOURCE;
+  const dismissElectronicDetails = (orderFormat === ORDER_FORMATS.PHYSICAL_RESOURCE) || (orderFormat === ORDER_FORMATS.OTHER);
 
   return {
     dismissCreateInventory,
