ca96b28bd27a17858342331ea801eed2fb02f06f
Mariia Aloshyna <Mariia_Aloshyna@epam.com>
2023-04-12 16:39:33 +0300

    UIDATIMP-1426: Fix lint warnings

--- src/routes/JobSummary/components/RecordsTable.test.js
+++ src/routes/JobSummary/components/RecordsTable.test.js
@@ -250,7 +250,7 @@ const renderRecordsTable = ({ isEdifactType = false }) => {
         source={source}
         resultCountIncrement={5}
         pageAmount={5}
-        history={{ push: noop }}
+        history={history}
         location="test location"
       />
     </BrowserRouter>
