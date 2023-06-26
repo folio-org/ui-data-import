163e684bc44d3420c6417959d9f2606e21da5bdc
Mariia Aloshyna <Mariia_Aloshyna@epam.com>
2023-06-26 17:13:51 +0300

    UIDATIMP-1442: Replace @babel/plugin-proposal- with @babel/plugin-transform

--- test/jest/babel.config.js
+++ test/jest/babel.config.js
@@ -4,10 +4,10 @@ module.exports = {
     ['@babel/preset-react', { runtime: 'automatic' }],
   ],
   plugins: [
-    ['@babel/plugin-proposal-decorators', { legacy: true }],
-    ['@babel/plugin-proposal-class-properties', { loose: true }],
-    ['@babel/plugin-proposal-private-methods', { loose: true }],
-    ['@babel/plugin-proposal-private-property-in-object', { loose: true }],
+    ['@babel/plugin-transform-decorators', { legacy: true }],
+    ['@babel/plugin-transform-class-properties', { loose: true }],
+    ['@babel/plugin-transform-private-methods', { loose: true }],
+    ['@babel/plugin-transform-private-property-in-object', { loose: true }],
     '@babel/plugin-transform-runtime',
   ],
 };
