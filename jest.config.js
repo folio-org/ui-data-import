0897f7b4983b251ab32575a34979477c21b7858a
Oleksandr Hladchenko1 <oleksandr_hladchenko1@epam.com>
2023-09-13 12:43:59 +0300

    UIDATIMP-1508: Add @folio/stripes-acq-components/jest.config

--- jest.config.js
+++ jest.config.js
@@ -1,1 +1,9 @@
-module.exports = require('@folio/jest-config-stripes');
+const config = require('@folio/jest-config-stripes');
+const acqConfigs = require('@folio/stripes-acq-components/jest.config');
+
+module.exports = {
+  ...config,
+  transform: {
+    ...acqConfigs.transform,
+  },
+};
