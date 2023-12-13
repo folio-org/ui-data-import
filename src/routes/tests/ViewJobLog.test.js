9a9a50323b80d0168ae0c603f2cac363787ef178
Oleksandr Hladchenko1 <oleksandr_hladchenko1@epam.com>
2023-12-13 15:23:34 +0200

    UIDATIMP-1542: Update tests in ViewJobLog.test.js

--- src/routes/tests/ViewJobLog.test.js
+++ src/routes/tests/ViewJobLog.test.js
@@ -124,6 +124,7 @@ jest.mock('../../hooks', () => ({
   useInvoiceLineByIdQuery: jest.fn(() => ({ isLoading: false, data: mockInvoiceLineJSONData })),
   useAuthoritiesByIdQuery: jest.fn(() => ({ isLoading: false, data: [mockAuthorityJSONData] })),
   useLocationsQuery: jest.fn(() => ({ isLoading: false, data: mockLocationsData })),
+  useIncomingRecordByIdQuery: jest.fn(() => ({ isLoading: false, data: '{ test: "test" }' })),
 }));
 
 const renderViewJobLog = () => {
@@ -157,14 +158,14 @@ describe('View job log page', () => {
         expect(queryByText('1 (Test record title)')).toBeDefined();
       });
 
-      it('"SRS MARC" tab should be active by default', () => {
+      it('"Incoming Record" tab should be active by default', () => {
         const { getByRole } = renderViewJobLog();
-        const srsMarcBibTabElement = getByRole('tab', {
-          name: 'SRS MARC',
+        const IncomingRecordTabElement = getByRole('tab', {
+          name: 'Incoming record',
           selected: true,
         });
 
-        expect(srsMarcBibTabElement).toBeDefined();
+        expect(IncomingRecordTabElement).toBeDefined();
       });
     });
 
@@ -204,7 +205,7 @@ describe('View job log page', () => {
     it('should have 7 tabs', () => {
       const { getAllByRole } = renderViewJobLog();
 
-      expect(getAllByRole('tab').length).toEqual(7);
+      expect(getAllByRole('tab').length).toEqual(8);
     });
   });
 
@@ -230,10 +231,15 @@ describe('View job log page', () => {
       });
 
       it('should display SRS MARC JSON details on the screen', () => {
-        const { container } = renderViewJobLog();
+        const {
+          container,
+          getByText,
+        } = renderViewJobLog();
+
+        fireEvent.click(getByText('Instance'));
         const codeElement = container.querySelector('code.info');
 
-        expect(JSON.parse(codeElement.textContent)).toEqual(mockSRSMARCBibMARCJSONData);
+        expect(JSON.parse(codeElement.textContent)).toEqual({ test: 'test' });
       });
 
       describe('and Instance tag is active', () => {
