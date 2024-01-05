5a4d7e6e46ac295047ad292d1827418c23c32405
Mariia Aloshyna <55138456+mariia-aloshyna@users.noreply.github.com>
2024-01-05 22:28:59 +0200

    UIDATIMP-1542: Add JSON tab for incoming record to Data Import log JSON screen (PR to temporary branch) (#1530)

---
+++ src/hooks/tests/useIncomingRecordByIdQuery.test.js
@@ -0,0 +1,33 @@
+import {
+  waitFor,
+  renderHook,
+} from '@folio/jest-config-stripes/testing-library/react';
+
+import '../../../test/jest/__mock__';
+
+import { useOkapiKy } from '@folio/stripes/core';
+
+import { useIncomingRecordByIdQuery } from '../useIncomingRecordByIdQuery';
+import { queryClientWrapper } from '../../../test/jest/helpers';
+
+const mockIncomingRecord = { id: 'incomingRecordId' };
+
+describe('useIncomingRecordByIdQuery', () => {
+  beforeEach(() => {
+    useOkapiKy.mockClear().mockReturnValue({
+      get: () => {
+        return ({
+          json: () => Promise.resolve({ parsedRecordContent: mockIncomingRecord }),
+        });
+      },
+    });
+  });
+
+  it('should fetch incoming record', async () => {
+    const { result } = renderHook(() => useIncomingRecordByIdQuery('incomingRecordId'), { wrapper: queryClientWrapper });
+
+    await waitFor(() => {
+      expect(result.current.data).toEqual(mockIncomingRecord);
+    });
+  });
+});
