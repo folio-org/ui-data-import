18555bf146af5849e7ba6210f1fc0c3b9105703f
Mariia Aloshyna <55138456+mariia-aloshyna@users.noreply.github.com>
2024-01-19 18:00:35 +0200

    UIDATIMP-1558: Make changes in requests for Log Details (#1531)

--- src/routes/JobSummary/components/RecordsTable.test.js
+++ src/routes/JobSummary/components/RecordsTable.test.js
@@ -18,145 +18,121 @@ window.open = jest.fn();
 window.open.mockReturnValue({ focus: jest.fn() });
 const history = createMemoryHistory();
 
-const firstRecordJobExecutionId = faker.random.uuid();
-
-const jobLogEntriesRecords = [{
-  sourceRecordActionStatus: 'CREATED',
-  sourceRecordType: 'MARC_BIBLIOGRAPHIC',
-  instanceActionStatus: 'CREATED',
-  sourceRecordTitle: 'Test record 1',
-  jobExecutionId: firstRecordJobExecutionId,
-  sourceRecordId: '59138d56-bc81-4f66-9f72-f57f53621111',
-  sourceRecordOrder: '0',
-}, {
-  sourceRecordActionStatus: 'CREATED',
-  sourceRecordType: 'MARC_BIBLIOGRAPHIC',
-  instanceActionStatus: 'CREATED',
-  sourceRecordTitle: 'Test record 2',
-  jobExecutionId: firstRecordJobExecutionId,
-  sourceRecordId: '59138d56-bc81-4f66-9f72-f57f53629646',
-  sourceRecordOrder: '0',
-}];
+const jobLogEntriesRecords = [
+  {
+    jobExecutionId: faker.random.uuid(),
+    sourceRecordId: '59138d56-bc81-4f66-9f72-f57f53621111',
+    sourceRecordOrder : 0,
+    sourceRecordTitle: 'Test record 1',
+    sourceRecordActionStatus: 'CREATED',
+    instanceActionStatus: 'CREATED',
+    error: '',
+    relatedInstanceInfo: {
+      actionStatus: 'CREATED',
+      idList: ['720031b9-a792-4936-963c-a7b63fb96574'],
+      hridList: ['in00000000014'],
+      error: '',
+    },
+    relatedHoldingsInfo: [],
+    relatedItemInfo: [],
+    relatedAuthorityInfo: {
+      idList: [],
+      hridList: [],
+    },
+    relatedPoLineInfo: {
+      idList: [],
+      hridList: [],
+    },
+    relatedInvoiceInfo: {
+      idList: [],
+      hridList: [],
+    },
+    relatedInvoiceLineInfo: { },
+  },
+  {
+    jobExecutionId: faker.random.uuid(),
+    sourceRecordId: '59138d56-bc81-4f66-9f72-f57f53629646',
+    sourceRecordOrder : 4,
+    sourceRecordTitle: 'Test record 2',
+    sourceRecordActionStatus: 'CREATED',
+    instanceActionStatus: 'CREATED',
+    error: '',
+    relatedInstanceInfo: {
+      actionStatus: 'CREATED',
+      idList: ['720031b9-a792-4936-963c-a7b63fb96574'],
+      hridList: ['in00000000014'],
+      error: '',
+    },
+    relatedHoldingsInfo: [{
+      actionStatus: 'CREATED',
+      id: 'f648c370-d9d6-432c-a502-b8eb718f867c',
+      permanentLocationId: '53cf956f-c1df-410b-8bea-27f712cca7c0',
+      hrid: 'ho00000000017',
+      error: '',
+    }, {
+      actionStatus: 'CREATED',
+      id: '5cadf17f-eb72-475c-a2e0-7e56f54f0000',
+      permanentLocationId: '758258bc-ecc1-41b8-abca-f7b610822ffd',
+      hrid: 'ho00000000018',
+      error: '',
+    }, {
+      actionStatus: 'DISCARDED',
+      id: '5cadf17f-eb72-475c-a2e0-7e56f54fd3b4',
+      permanentLocationId: 'fcd64ce1-6995-48f0-840e-89ffa2288371',
+      hrid: 'ho00000000014',
+      error: '',
+    }],
+    relatedItemInfo: [{
+      actionStatus: 'CREATED',
+      id: '37f20cb1-f60c-4195-80a1-00a16c4af5cb',
+      hrid: 'it00000000015',
+      holdingsId: 'f648c370-d9d6-432c-a502-b8eb718f867c',
+      error: '',
+    }, {
+      actionStatus: 'DISCARDED',
+      id: 'ccd19bf0-add1-46bb-899b-c457fd448b51',
+      hrid: 'it00000000016',
+      holdingsId: 'f648c370-d9d6-432c-a502-b8eb718f867c',
+      error: 'test error',
+    }, {
+      actionStatus: 'CREATED',
+      id: 'ccd19bf0-add1-46bb-899b-c457fd441111',
+      hrid: 'it00000000019',
+      holdingsId: '5cadf17f-eb72-475c-a2e0-7e56f54f0000',
+      error: '',
+    }, {
+      actionStatus: 'CREATED',
+      id: '3bcfe427-a747-405f-b3f3-1d842ffb2222',
+      hrid: 'it00000000018',
+      holdingsId: '5cadf17f-eb72-475c-a2e0-7e56f54f0000',
+      error: '',
+    }, {
+      actionStatus: 'CREATED',
+      id: '3bcfe427-a747-405f-b3f3-1d842ffb49c6',
+      hrid: 'it00000000014',
+      holdingsId: 'f648c370-d9d6-432c-a502-b8eb718f867c',
+      error: '',
+    }],
+    relatedAuthorityInfo: {
+      idList: [],
+      hridList: [],
+    },
+    relatedPoLineInfo: {
+      idList: [],
+      hridList: [],
+    },
+    relatedInvoiceInfo: {
+      idList: [],
+      hridList: [],
+    },
+    relatedInvoiceLineInfo: { },
+  },
+];
 
 const jobLogEntriesResources = {
   jobLogEntries: {
     records: jobLogEntriesRecords,
     other: { totalRecords: jobLogEntriesRecords.length }
-  },
-};
-
-const jobLogRecords = [{
-  jobExecutionId: '467d7627-c0db-4fb7-b333-4b4983dbf781',
-  sourceRecordId: '59138d56-bc81-4f66-9f72-f57f53621111',
-  sourceRecordOrder : 0,
-  sourceRecordTitle: 'Test record 1',
-  sourceRecordActionStatus: 'CREATED',
-  instanceActionStatus: 'CREATED',
-  error: '',
-  relatedInstanceInfo: {
-    actionStatus: 'CREATED',
-    idList: ['720031b9-a792-4936-963c-a7b63fb96574'],
-    hridList: ['in00000000014'],
-    error: '',
-  },
-  relatedHoldingsInfo: [],
-  relatedItemInfo: [],
-  relatedAuthorityInfo: {
-    idList: [],
-    hridList: [],
-  },
-  relatedPoLineInfo: {
-    idList: [],
-    hridList: [],
-  },
-  relatedInvoiceInfo: {
-    idList: [],
-    hridList: [],
-  },
-  relatedInvoiceLineInfo: { },
-}, {
-  jobExecutionId: '467d7627-c0db-4fb7-b333-4b4983dbf781',
-  sourceRecordId: '59138d56-bc81-4f66-9f72-f57f53629646',
-  sourceRecordOrder : 4,
-  sourceRecordTitle: 'Test record 2',
-  sourceRecordActionStatus: 'CREATED',
-  instanceActionStatus: 'CREATED',
-  error: '',
-  relatedInstanceInfo: {
-    actionStatus: 'CREATED',
-    idList: ['720031b9-a792-4936-963c-a7b63fb96574'],
-    hridList: ['in00000000014'],
-    error: '',
-  },
-  relatedHoldingsInfo: [{
-    actionStatus: 'CREATED',
-    id: 'f648c370-d9d6-432c-a502-b8eb718f867c',
-    permanentLocationId: '53cf956f-c1df-410b-8bea-27f712cca7c0',
-    hrid: 'ho00000000017',
-    error: '',
-  }, {
-    actionStatus: 'CREATED',
-    id: '5cadf17f-eb72-475c-a2e0-7e56f54f0000',
-    permanentLocationId: '758258bc-ecc1-41b8-abca-f7b610822ffd',
-    hrid: 'ho00000000018',
-    error: '',
-  }, {
-    actionStatus: 'DISCARDED',
-    id: '5cadf17f-eb72-475c-a2e0-7e56f54fd3b4',
-    permanentLocationId: 'fcd64ce1-6995-48f0-840e-89ffa2288371',
-    hrid: 'ho00000000014',
-    error: '',
-  }],
-  relatedItemInfo: [{
-    actionStatus: 'CREATED',
-    id: '37f20cb1-f60c-4195-80a1-00a16c4af5cb',
-    hrid: 'it00000000015',
-    holdingsId: 'f648c370-d9d6-432c-a502-b8eb718f867c',
-    error: '',
-  }, {
-    actionStatus: 'DISCARDED',
-    id: 'ccd19bf0-add1-46bb-899b-c457fd448b51',
-    hrid: 'it00000000016',
-    holdingsId: 'f648c370-d9d6-432c-a502-b8eb718f867c',
-    error: 'test error',
-  }, {
-    actionStatus: 'CREATED',
-    id: 'ccd19bf0-add1-46bb-899b-c457fd441111',
-    hrid: 'it00000000019',
-    holdingsId: '5cadf17f-eb72-475c-a2e0-7e56f54f0000',
-    error: '',
-  }, {
-    actionStatus: 'CREATED',
-    id: '3bcfe427-a747-405f-b3f3-1d842ffb2222',
-    hrid: 'it00000000018',
-    holdingsId: '5cadf17f-eb72-475c-a2e0-7e56f54f0000',
-    error: '',
-  }, {
-    actionStatus: 'CREATED',
-    id: '3bcfe427-a747-405f-b3f3-1d842ffb49c6',
-    hrid: 'it00000000014',
-    holdingsId: 'f648c370-d9d6-432c-a502-b8eb718f867c',
-    error: '',
-  }],
-  relatedAuthorityInfo: {
-    idList: [],
-    hridList: [],
-  },
-  relatedPoLineInfo: {
-    idList: [],
-    hridList: [],
-  },
-  relatedInvoiceInfo: {
-    idList: [],
-    hridList: [],
-  },
-  relatedInvoiceLineInfo: { },
-}];
-
-const jobLogResources = {
-  jobLog: {
-    records: jobLogRecords,
-    other: { totalRecords: jobLogRecords.length }
   },
 };
 
@@ -175,7 +151,6 @@ const locations = {
 
 const resources = {
   ...jobLogEntriesResources,
-  ...jobLogResources,
   locations,
 };
 
