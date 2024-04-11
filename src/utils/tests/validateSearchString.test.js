c69a58a29799ec5a783ef7f69f9f8e18d168b7a9
Oleksandr Hladchenko1 <oleksandr_hladchenko1@epam.com>
2024-04-11 17:25:46 +0300

    UIDATIMP-1605: Add test file

---
+++ src/utils/tests/validateSearchString.test.js
@@ -0,0 +1,14 @@
+import { validateSearchString } from '../validateSearchString';
+
+describe('validateSearchString function', () => {
+  it('should return valid search string', () => {
+    const searchString1 = 'test*';
+    const searchString2 = '"test"';
+
+    const expectedSearchString1 = 'test\\*';
+    const expectedSearchString2 = '\\"test\\"';
+
+    expect(validateSearchString(searchString1)).toBe(expectedSearchString1);
+    expect(validateSearchString(searchString2)).toBe(expectedSearchString2);
+  });
+});
