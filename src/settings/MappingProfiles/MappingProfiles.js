24384b97184429cb903a483bdd88006dc968626d
Oleksandr Hladchenko1 <oleksandr_hladchenko1@epam.com>
2024-04-15 12:51:49 +0300

    UIDATIMP-1613: Perform sorting by last name first, instead of first name in 'Updated by' column

--- src/settings/MappingProfiles/MappingProfiles.js
+++ src/settings/MappingProfiles/MappingProfiles.js
@@ -38,7 +38,7 @@ const sortMap = {
   folioRecord: 'existingRecordType',
   tags: 'tags.tagList',
   updated: 'metadata.updatedDate',
-  updatedBy: 'userInfo.firstName userInfo.lastName userInfo.userName',
+  updatedBy: 'userInfo.lastName userInfo.firstName userInfo.userName',
 };
 const defaultSort = 'name';
 
