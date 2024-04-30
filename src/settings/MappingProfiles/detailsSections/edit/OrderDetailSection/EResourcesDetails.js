f87194e4a13b6a3523a3374d8fea9f224514ad4e
Mariia_Aloshyna <Mariia_Aloshyna@epam.com>
2024-04-30 17:19:53 +0300

    UIDATIMP-1626: Update Order mapping rules to allow Material Type (e-resources details) mapping when Purchase Order Status set to 'Open'

--- src/settings/MappingProfiles/detailsSections/edit/OrderDetailSection/EResourcesDetails.js
+++ src/settings/MappingProfiles/detailsSections/edit/OrderDetailSection/EResourcesDetails.js
@@ -82,7 +82,7 @@ export const EResourcesDetails = ({
   useEffect(() => {
     if (dismissCreateInventory) {
       clearFieldValue({
-        paths: [E_RESOURCES_DETAILS_FIELDS_MAP.CREATE_INVENTORY, E_RESOURCES_DETAILS_FIELDS_MAP.MATERIAL_TYPE],
+        paths: [E_RESOURCES_DETAILS_FIELDS_MAP.CREATE_INVENTORY],
         setReferenceTables,
       });
     }
@@ -206,7 +206,7 @@ export const EResourcesDetails = ({
             setAcceptedValues={setReferenceTables}
             acceptedValuesPath={getAcceptedValuesPath(MATERIAL_TYPE_INDEX)}
             okapi={okapi}
-            disabled={dismissElectronicDetails || dismissCreateInventory}
+            disabled={dismissElectronicDetails}
           />
         </Col>
         <Col xs={3}>
