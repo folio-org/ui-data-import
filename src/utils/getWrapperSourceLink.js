fa4e768b97bc7e937cac491c381e959eb70e9007
Oleksandr Hladchenko1 <oleksandr_hladchenko1@epam.com>
2025-02-17 11:12:06 +0100

    UIDATIMP-1702: Revert orders requests

--- src/utils/getWrapperSourceLink.js
+++ src/utils/getWrapperSourceLink.js
@@ -5,8 +5,8 @@ import {
 
 export const getWrapperSourceLink = (key, resourceLimit = PER_REQUEST_LIMIT) => {
   const WRAPPER_SOURCE_LINKS = {
-    PREFIXES: `/orders/settings/prefixes?limit=${resourceLimit}&query=${FIND_ALL_CQL} sortby name`,
-    SUFFIXES: `/orders/settings/suffixes?limit=${resourceLimit}&query=${FIND_ALL_CQL} sortby name`,
+    PREFIXES: `/orders/configurations/prefixes?limit=${resourceLimit}&query=${FIND_ALL_CQL} sortby name`,
+    SUFFIXES: `/orders/configurations/suffixes?limit=${resourceLimit}&query=${FIND_ALL_CQL} sortby name`,
     ACQUISITIONS_UNITS: `/acquisitions-units/units?limit=${resourceLimit}&query=${FIND_ALL_CQL} sortby name`,
     ADDRESSES: `/settings/entries?limit=${resourceLimit}&query=(scope=tenant and key=tenant.addresses) sortBy value`,
     CONTRIBUTOR_NAME_TYPES: `/contributor-name-types?limit=${resourceLimit}&query=${FIND_ALL_CQL} sortby name`,
